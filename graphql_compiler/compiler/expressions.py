26527cf08bdb6b29cce0dd789ea883b12dd4e5c3
cclauss <cclauss@bluewin.ch>
2017-07-20 23:01:00 +0200

    Update expressions.py

--- graphql_compiler/compiler/expressions.py
+++ graphql_compiler/compiler/expressions.py
@@ -1,11 +1,11 @@
 # Copyright 2017 Kensho Technologies, Inc.
 from graphql import GraphQLList, GraphQLNonNull
 
+from ..compat import basestring, unicode
 from ..exceptions import GraphQLCompilationError
 from ..schema import GraphQLDate, GraphQLDateTime
 from .helpers import (CompilerEntity, Location, ensure_unicode_string, is_graphql_type,
                       safe_quoted_string, strip_non_null_from_type, validate_safe_string)
-import ..compat
 
 
 # Since MATCH uses $-prefixed keywords to indicate special values,
