e02a324713545fdbf16126dee2bacd759ec1f8e4
benlongo <benlongo9807@gmail.com>
2017-07-20 21:15:28 -0400

    Test output name starting with digit.

--- graphql_compiler/tests/test_ir_generation_errors.py
+++ graphql_compiler/tests/test_ir_generation_errors.py
@@ -233,6 +233,12 @@ class IrGenerationErrorTests(unittest.TestCase):
             }
         }''')
 
+        output_with_name_starting_with_digit = (GraphQLCompilationError, '''{
+            Animal @filter(op_name: "name_or_alias", value: ["$animal_name"]) {
+                name @output(out_name: "1uuid")
+            }
+        }''')
+
         output_with_duplicated_name = (GraphQLCompilationError, '''{
             Animal @filter(op_name: "name_or_alias", value: ["$animal_name"]) {
                 uuid @output(out_name: "uuid")
@@ -252,7 +258,8 @@ class IrGenerationErrorTests(unittest.TestCase):
                                         output_without_name,
                                         output_with_duplicated_name,
                                         output_with_illegal_name,
-                                        output_with_empty_name,):
+                                        output_with_empty_name,
+                                        output_with_name_starting_with_digit):
             with self.assertRaises(expected_error):
                 graphql_to_ir(self.schema, graphql)
 
