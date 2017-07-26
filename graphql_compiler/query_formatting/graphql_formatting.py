f20af36c0ba156243c4436c12c3ae0fca98de6b8
benlongo <benlongo9807@gmail.com>
2017-07-26 14:13:19 -0400

    Add rationale.

--- graphql_compiler/query_formatting/graphql_formatting.py
+++ graphql_compiler/query_formatting/graphql_formatting.py
@@ -1,5 +1,4 @@
 # Copyright 2017 Kensho Technologies, Inc.
-import graphql
 from graphql import parse
 from graphql.language.printer import PrintingVisitor, wrap, join
 from graphql.language.visitor import visit
@@ -8,53 +7,55 @@ from ..schema import DIRECTIVES
 
 def pretty_print_graphql(query, spaces=4):
     """Take a GraphQL query, pretty print it, and return it."""
-    # Use four spaces for indentation, to make it easier up to edit in Python
-    # source files.
-    old_indent = graphql.language.printer.indent
+    # The graphql-core printer module has an indent function outside of the
+    # PrintingVisitor class which has two spaces hard coded. Ideally, it would
+    # be a member of the class and we could simply override it in our custom
+    # printer. To work around this we can set the function inside the module
+    # forcefully. This is undesireable as it has a global effect and relies on
+    # the implementation details of the print_ast() function, but has the
+    # advantage of only modifying the leave_Directive() method on the class as
+    # opposed to copying the entire class.
+    import graphql
+
+    def _indent_override(spaces):
+        def indent(maybe_str):
+            if maybe_str:
+                return maybe_str.replace('\n', '\n' + ' ' * spaces)
+            return maybe_str
+        return indent
     graphql.language.printer.indent = _indent_override(spaces)
-
-    output = visit(parse(query), CustomPrinter(spaces))
-
-    graphql.language.printer.indent = old_indent
-
+    
+    output = visit(parse(query), CustomPrintingVisitor())
     return output
 
 
-def _indent_override(spaces):
-    def indent(maybe_str):
-        if maybe_str:
-            return maybe_str.replace('\n', '\n' + ' ' * spaces)
-        return maybe_str
-    return indent
-
-
-class CustomPrinter(PrintingVisitor):
-    def __init__(self, spaces=2):
-        self._spaces = spaces
-
-    # @filter directives are much easier to read if the operation comes before
-    # the values. The default AST printer outputs the values
-    # before the operation, so the filtering operation ends up written in
-    # postfix order.
+class CustomPrintingVisitor(PrintingVisitor):
+    # Directives are easier to read if they are in the order in which we defined
+    # them in the schema. For example, @filter directives are much easier to
+    # read if the operation comes before the values. The arguments of the
+    # directives specified in the schema are defined as OrderedDicts which
+    # allows us to sort the provided arguments to match.
     def leave_Directive(self, node, *args):
         args = node.arguments
 
-        def _arg_order(arg, directive):
+        def _arg_index(arg, directive):
             defined_arg_names = directive.args.keys()
             # Taking [0] is ok here because the graphql parser checks for the
             # existence of ':' in directive arguments
-            arg_name = arg.split(':')[0]
+            arg_name = arg.split(':', 1)[0]
             try:
                 return defined_arg_names.index(arg_name)
             except ValueError:
                 # This argument name isn't defined in our schema but we should
-                # still pretty print it
+                # still pretty print it anyway
                 return -1
 
         try:
             directive = next(d for d in DIRECTIVES if d.name == node.name)
-            args = list(sorted(args, key=lambda a: _arg_order(a, directive)))
+            args = list(sorted(args, key=lambda a: _arg_index(a, directive)))
         except StopIteration:
+            # The directive wasn't defined in our schema, use whatever order
+            # the args appeared in within the query
             pass
 
         return '@' + node.name + wrap('(', join(args, ', '), ')')
