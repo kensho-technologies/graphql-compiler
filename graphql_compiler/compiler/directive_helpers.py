2fe62f0c1f19e783b1b5cdb3b956b702cfe8e114
Amartya Shankha Biswas <shankha.biswas@kensho.com>
2018-04-03 15:22:23 -0400

    Removed commented non-fold context detection from directive_helpers.validate_vertex_field_directive_in_context.

--- graphql_compiler/compiler/directive_helpers.py
+++ graphql_compiler/compiler/directive_helpers.py
@@ -189,9 +189,6 @@ def validate_vertex_field_directive_in_context(location, directives, context):
     recurse_directive = directives.get('recurse', None)
 
     fold_context = 'fold' in context
-    # optional_context = 'optional' in context
-    # output_context = 'output' in context
-    # recurse_context = 'recurse' in context
 
     if fold_directive and fold_context:
         raise GraphQLCompilationError(u'@fold is not allowed within a @fold traversal! '
