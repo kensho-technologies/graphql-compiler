0d32974af9d651e97eb66ad64379bef101ea15c8
Amartya Shankha Biswas <shankha.biswas@kensho.com>
2018-05-14 12:25:02 -0400

    Addressing comments.

--- graphql_compiler/tests/test_testing_invariants.py
+++ graphql_compiler/tests/test_testing_invariants.py
@@ -7,20 +7,25 @@ import graphql_compiler.tests.test_input_data as test_input_data
 
 
 def get_function_names_from_module(module):
-    return [
+    """Return a set of function names present in a given module."""
+    return {
         member
-        for (member, member_type) in getmembers(module)
+        for member, member_type in getmembers(module)
         if isfunction(member_type)
-    ]
+    }
 
 
-def get_function_names_from_class(test_class):
+def get_test_function_names_from_class(test_class):
+    """Return a set of test function names present in a given TestCase class."""
+    if not issubclass(test_class, unittest.TestCase):
+        raise AssertionError(u'Received non-test class {} as input.'
+                             .format(test_class))
     member_dict = test_class.__dict__
-    return set(
+    return {
         member
         for member in member_dict
         if isfunction(member_dict[member]) and member[:5] == 'test_'
-    )
+    }
 
 
 IGNORED_FUNCTIONS = ['namedtuple']
@@ -29,19 +34,19 @@ IGNORED_FUNCTIONS = ['namedtuple']
 class TestingInvariants(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
-        input_names_list = get_function_names_from_module(test_input_data)
-        self.expected_test_functions_list = [
+        input_names = get_function_names_from_module(test_input_data)
+        self.expected_test_functions = {
             'test_' + input_name
-            for input_name in input_names_list
+            for input_name in input_names
             if input_name not in IGNORED_FUNCTIONS
-        ]
+        }
 
     def test_ir_generation_test_invariants(self):
         # Importing IrGenerationTests globally would expose them to py.test a second time.
         # We import them here so that these tests are not run again.
         from .test_ir_generation import IrGenerationTests
-        ir_generation_test_names = get_function_names_from_class(IrGenerationTests)
-        for expected_test_function_name in self.expected_test_functions_list:
+        ir_generation_test_names = get_test_function_names_from_class(IrGenerationTests)
+        for expected_test_function_name in self.expected_test_functions:
             if expected_test_function_name not in ir_generation_test_names:
                 raise AssertionError(u'Test case "{}" not found in ir_generation_tests.py.'
                                      .format(expected_test_function_name))
@@ -50,8 +55,8 @@ class TestingInvariants(unittest.TestCase):
         # Importing CompilerTests globally would expose them to py.test a second time.
         # We import them here so that these tests are not run again.
         from .test_compiler import CompilerTests
-        compiler_test_names = get_function_names_from_class(CompilerTests)
-        for expected_test_function_name in self.expected_test_functions_list:
+        compiler_test_names = get_test_function_names_from_class(CompilerTests)
+        for expected_test_function_name in self.expected_test_functions:
             if expected_test_function_name not in compiler_test_names:
                 raise AssertionError(u'Test case "{}" not found in compiler_tests.py.'
                                      .format(expected_test_function_name))
