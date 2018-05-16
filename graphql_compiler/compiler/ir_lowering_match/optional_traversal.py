4b88d2064e5c9c4b8082be86a127c872b6f9f187
Amartya Shankha Biswas <shankha.biswas@kensho.com>
2018-05-16 10:38:48 -0400

    Fixed FoldScopeLocation Error

--- graphql_compiler/compiler/ir_lowering_match/optional_traversal.py
+++ graphql_compiler/compiler/ir_lowering_match/optional_traversal.py
@@ -241,7 +241,7 @@ def prune_non_existent_outputs(compound_match_query):
                     if location_name in present_locations:
                         raise AssertionError(u'Folded output location {} was found in '
                                              u'present_locations: {}'
-                                             .format(expression.location, present_locations))
+                                             .format(base_location, present_locations))
                     new_output_fields[output_name] = expression
                 elif isinstance(expression, TernaryConditional):
                     # A TernaryConditional indicates that this output is within some optional scope.
