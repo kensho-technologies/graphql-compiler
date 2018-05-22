fa5738d782f49f8f44005b52ab4ed7ad7d5c0138
Amartya Shankha Biswas <shankha.biswas@kensho.com>
2018-05-22 12:14:15 -0400

    Fixed commands import

--- graphql_compiler/tests/test_data_tools/animal_tool.py
+++ graphql_compiler/tests/test_data_tools/animal_tool.py
@@ -5,7 +5,7 @@ from os import path
 def generate_animals(client):
     """Create animal vertices from the file."""
     project_root = path.dirname(path.dirname(path.abspath(__file__)))
-    sql_files = glob(path.join(project_root, 'test_data_tools/create_db/*.sql'))
+    sql_files = glob(path.join(project_root, 'test_data_tools/data/*.sql'))
     for filepath in sql_files:
         with open(filepath) as f:
             for command in f.readlines():
