46957ffe2c9e673a9d983559b52cf068b0c8c88b
Amartya Shankha Biswas <shankha.biswas@kensho.com>
2018-05-22 11:40:09 -0400

    Generated snapshots file

---
+++ graphql_compiler/tests/snapshot_tests/snapshots/snap_test_orientdb_match_query.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['OrientdbMatchQueryTests::test_immediate_output 1'] = [
+]
+
+snapshots['OrientdbMatchQueryTests::test_optional_and_deep_traverse 1'] = [
+]
+
+snapshots['OrientdbMatchQueryTests::test_optional_traverse_after_mandatory_traverse 1'] = [
+]
+
+snapshots['OrientdbMatchQueryTests::test_traverse_and_output 1'] = [
+]
