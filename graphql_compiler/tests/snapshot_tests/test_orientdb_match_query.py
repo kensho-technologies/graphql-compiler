6a2b50bcde1ca0651eefcc2bef2600f6bc17972d
Amartya Shankha Biswas <shankha.biswas@kensho.com>
2018-07-17 18:06:42 -0400

    Adding all non parametrized test cases

--- graphql_compiler/tests/snapshot_tests/test_orientdb_match_query.py
+++ graphql_compiler/tests/snapshot_tests/test_orientdb_match_query.py
@@ -66,6 +66,485 @@ class OrientDBMatchQueryTests(TestCase):
         self.schema = get_schema()
 
     @pytest.mark.usefixtures('graph_client')
+    def test_immediate_output(self):
+        test_data = test_input_data.immediate_output()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_traverse_and_output(self):
+        test_data = test_input_data.traverse_and_output()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_optional_traverse_after_mandatory_traverse(self):
+        test_data = test_input_data.optional_traverse_after_mandatory_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_optional_and_deep_traverse(self):
+        test_data = test_input_data.optional_and_deep_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_fold_on_output_variable(self):
+        test_data = test_input_data.fold_on_output_variable()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_immediate_output(self):
+        test_data = test_input_data.immediate_output()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_immediate_output_custom_scalars(self):
+        test_data = test_input_data.immediate_output_custom_scalars()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_traverse_and_output(self):
+        test_data = test_input_data.traverse_and_output()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_optional_traverse_after_mandatory_traverse(self):
+        test_data = test_input_data.optional_traverse_after_mandatory_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_filter_on_optional_variable_equality(self):
+        test_data = test_input_data.filter_on_optional_variable_equality()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_filter_on_optional_variable_name_or_alias(self):
+        test_data = test_input_data.filter_on_optional_variable_name_or_alias()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_simple_fragment(self):
+        test_data = test_input_data.simple_fragment()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    # @pytest.mark.usefixtures('graph_client')
+    # def test_simple_union(self):
+    #     test_data = test_input_data.simple_union()
+    #     sample_parameters = {}
+
+    #     rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+    #     self.assertMatchSnapshot(rows)
+
+    # @pytest.mark.usefixtures('graph_client')
+    # def test_optional_on_union(self):
+    #     test_data = test_input_data.optional_on_union()
+    #     sample_parameters = {}
+
+    #     rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+    #     self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_typename_output(self):
+        test_data = test_input_data.typename_output()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_simple_recurse(self):
+        test_data = test_input_data.simple_recurse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    # @pytest.mark.usefixtures('graph_client')
+    # def test_recurse_within_fragment(self):
+    #     test_data = test_input_data.recurse_within_fragment()
+    #     sample_parameters = {}
+
+    #     rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+    #     self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_recurse_with_immediate_type_coercion(self):
+        test_data = test_input_data.recurse_with_immediate_type_coercion()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_in_collection_op_filter_with_tag(self):
+        test_data = test_input_data.in_collection_op_filter_with_tag()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_in_collection_op_filter_with_optional_tag(self):
+        test_data = test_input_data.in_collection_op_filter_with_optional_tag()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_intersects_op_filter_with_tag(self):
+        test_data = test_input_data.intersects_op_filter_with_tag()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_intersects_op_filter_with_optional_tag(self):
+        test_data = test_input_data.intersects_op_filter_with_optional_tag()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_contains_op_filter_with_tag(self):
+        test_data = test_input_data.contains_op_filter_with_tag()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_contains_op_filter_with_optional_tag(self):
+        test_data = test_input_data.contains_op_filter_with_optional_tag()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_has_substring_op_filter_with_optional_tag(self):
+        test_data = test_input_data.has_substring_op_filter_with_optional_tag()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_fold_on_output_variable(self):
+        test_data = test_input_data.fold_on_output_variable()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_fold_after_traverse(self):
+        test_data = test_input_data.fold_after_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_fold_and_traverse(self):
+        test_data = test_input_data.fold_and_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_fold_and_deep_traverse(self):
+        test_data = test_input_data.fold_and_deep_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_traverse_and_fold_and_traverse(self):
+        test_data = test_input_data.traverse_and_fold_and_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_multiple_outputs_in_same_fold(self):
+        test_data = test_input_data.multiple_outputs_in_same_fold()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_multiple_outputs_in_same_fold_and_traverse(self):
+        test_data = test_input_data.multiple_outputs_in_same_fold_and_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_multiple_folds(self):
+        test_data = test_input_data.multiple_folds()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_multiple_folds_and_traverse(self):
+        test_data = test_input_data.multiple_folds_and_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_fold_date_and_datetime_fields(self):
+        test_data = test_input_data.fold_date_and_datetime_fields()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_coercion_to_union_base_type_inside_fold(self):
+        test_data = test_input_data.coercion_to_union_base_type_inside_fold()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_no_op_coercion_inside_fold(self):
+        test_data = test_input_data.no_op_coercion_inside_fold()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_coercion_on_interface_within_fold_scope(self):
+        test_data = test_input_data.coercion_on_interface_within_fold_scope()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_coercion_on_interface_within_fold_traversal(self):
+        test_data = test_input_data.coercion_on_interface_within_fold_traversal()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_coercion_on_union_within_fold_scope(self):
+        test_data = test_input_data.coercion_on_union_within_fold_scope()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_optional_and_traverse(self):
+        test_data = test_input_data.optional_and_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_optional_and_deep_traverse(self):
+        test_data = test_input_data.optional_and_deep_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_traverse_and_optional_and_traverse(self):
+        test_data = test_input_data.traverse_and_optional_and_traverse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_coercion_on_interface_within_optional_traversal(self):
+        test_data = test_input_data.coercion_on_interface_within_optional_traversal()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_filter_on_optional_traversal_equality(self):
+        test_data = test_input_data.filter_on_optional_traversal_equality()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_filter_on_optional_traversal_name_or_alias(self):
+        test_data = test_input_data.filter_on_optional_traversal_name_or_alias()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_simple_optional_recurse(self):
+        test_data = test_input_data.simple_optional_recurse()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_multiple_traverse_within_optional(self):
+        test_data = test_input_data.multiple_traverse_within_optional()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_optional_and_fold(self):
+        test_data = test_input_data.optional_and_fold()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_fold_and_optional(self):
+        test_data = test_input_data.fold_and_optional()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_optional_traversal_and_fold_traversal(self):
+        test_data = test_input_data.optional_traversal_and_fold_traversal()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_fold_traversal_and_optional_traversal(self):
+        test_data = test_input_data.fold_traversal_and_optional_traversal()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+    @pytest.mark.usefixtures('graph_client')
+    def test_coercion_and_filter_with_tag(self):
+        test_data = test_input_data.coercion_and_filter_with_tag()
+        sample_parameters = {}
+
+        rows = execute_graphql(self.schema, test_data, self.graph_client, sample_parameters)
+
+        self.assertMatchSnapshot(rows)
+
+
+    @pytest.mark.usefixtures('graph_client')
     def test_traverse_filter_and_output(self):
         test_data = test_input_data.traverse_filter_and_output()
         sample_parameters = {'wanted': 'Nazgul__2'}
