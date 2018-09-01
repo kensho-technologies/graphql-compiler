ecb13e4498986992c5fcd5e69693ad3ecb08663b
Amartya Shankha Biswas <asbiswas@mit.edu>
2018-09-01 13:31:44 -0400

    Removed pdb.

--- graphql_compiler/tests/test_helpers.py
+++ graphql_compiler/tests/test_helpers.py
@@ -39,7 +39,6 @@ def compare_ir_blocks(test_case, expected_blocks, received_blocks):
         expected = expected_blocks[i]
         received = received_blocks[i]
         if expected != received:
-            __import__('pdb').set_trace()
         test_case.assertEqual(expected, received,
                               msg=u'Blocks at position {} were different: {} vs {}\n\n'
                                   u'{}'.format(i, expected, received, mismatch_message))
