e2b618cf29d33e3716e5f289d24a82ae90d5b26e
jeremy.meulemans <jeremy.meulemans@kensho.com>
2018-12-20 09:37:34 -0500

    Simplifying OrientDB integration data

--- graphql_compiler/tests/test_data_tools/integration_data/create_orientdb_integration.sql
+++ graphql_compiler/tests/test_data_tools/integration_data/create_orientdb_integration.sql
@@ -1,4 +1,4 @@
-create vertex Animal set alias = ['unused'], birthday = DATE("2010-07-19 00:00:00"), color = 'brown', name = 'Animal 1', net_worth = Decimal('100.0'), uuid = 'cfc6e625-8594-0927-468f-f53d864a7a50'
-create vertex Animal set alias = ['unused'], birthday = DATE("2010-07-19 00:00:00"), color = 'brown', name = 'Animal 2', net_worth = Decimal('100.0'), uuid = 'cfc6e625-8594-0927-468f-f53d864a7a51'
-create vertex Animal set alias = ['unused'], birthday = DATE("2018-05-19 00:00:00"), color = 'brown', name = 'Animal 3', net_worth = Decimal('100.0'), uuid = 'cfc6e625-8594-0927-468f-f53d864a7a52'
-create vertex Animal set alias = ['unused'], birthday = DATE("2018-07-19 00:00:00"), color = 'brown', name = 'Animal 4', net_worth = Decimal('100.0'), uuid = 'cfc6e625-8594-0927-468f-f53d864a7a53'
+create vertex Animal set name = 'Animal 1', uuid = 'cfc6e625-8594-0927-468f-f53d864a7a50'
+create vertex Animal set name = 'Animal 2', uuid = 'cfc6e625-8594-0927-468f-f53d864a7a51'
+create vertex Animal set name = 'Animal 3', uuid = 'cfc6e625-8594-0927-468f-f53d864a7a52'
+create vertex Animal set name = 'Animal 4', uuid = 'cfc6e625-8594-0927-468f-f53d864a7a53'
