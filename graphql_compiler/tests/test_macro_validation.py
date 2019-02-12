713ab27f95add148ee2d96b12cd14e72a9cffc5b
Bojan Serafimov <bojan@kensho.com>
2019-02-12 14:25:06 -0500

    Add tests

--- graphql_compiler/tests/test_macro_validation.py
+++ graphql_compiler/tests/test_macro_validation.py
@@ -256,6 +256,50 @@ class MacroValidationTests(unittest.TestCase):
                                 args, self.type_equivalence_hints)
 
     @pytest.mark.skip(reason='not implemented')
+    def test_macro_edge_duplicating_real_edge_name(self):
+        query = '''{
+            Species @macro_edge_definition(name: "out_Species_Eats") {
+                out_Entity_Related {
+                    out_Entity_Related @macro_edge_target {
+                        ... on Animal @macro_edge_target {
+                            uuid
+                        }
+                    }
+                }
+            }
+        }'''
+        args = {}
+
+        macro_registry = create_macro_registry()
+        register_macro_edge(macro_registry, self.schema, query,
+                            args, self.type_equivalence_hints)
+        with self.assertRaises(AssertionError):
+            register_macro_edge(macro_registry, self.schema, query,
+                                args, self.type_equivalence_hints)
+
+    @pytest.mark.skip(reason='not implemented')
+    def test_macro_edge_duplicating_real_edge_name_at_target(self):
+        query = '''{
+            Species @macro_edge_definition(name: "out_Animal_ParentOf") {
+                out_Entity_Related {
+                    out_Entity_Related @macro_edge_target {
+                        ... on Animal @macro_edge_target {
+                            uuid
+                        }
+                    }
+                }
+            }
+        }'''
+        args = {}
+
+        macro_registry = create_macro_registry()
+        register_macro_edge(macro_registry, self.schema, query,
+                            args, self.type_equivalence_hints)
+        with self.assertRaises(AssertionError):
+            register_macro_edge(macro_registry, self.schema, query,
+                                args, self.type_equivalence_hints)
+
+    @pytest.mark.skip(reason='not implemented')
     def test_macro_edge_duplicate_definition_on_target(self):
         # Future-proofing, so that we can reverse macro edges
         query = '''{
