40f1842f4192a278ebc8183e9d982829e5e887ab
Pedro Mantica <pedro.mantica@kensho.com>
2019-02-28 14:51:17 -0500

    Added fully generated schema

--- graphql_compiler/tests/test_helpers.py
+++ graphql_compiler/tests/test_helpers.py
@@ -94,168 +94,157 @@ def get_schema():
     # Instead, it aims to capture as many real-world edge cases as possible,
     # without requiring a massive number of types and interfaces.
     schema_text = '''
-          schema {
-  query: RootSchemaQuery
-}
-
-directive @filter(op_name: String!, value: [String!]!) on FIELD | INLINE_FRAGMENT
-
-directive @tag(tag_name: String!) on FIELD
-
-directive @output(out_name: String!) on FIELD
-
-directive @output_source on FIELD
-
-directive @optional on FIELD
-
-directive @recurse(depth: Int!) on FIELD
-
-directive @fold on FIELD
-
-type Animal implements Entity, UniquelyIdentifiable {
-  _x_count: Int
-  alias: [String]
-  birthday: Date
-  color: String
-  description: String
-  in_Animal_ParentOf: [Animal]
-  in_Entity_Related: [Entity]
-  name: String
-  net_worth: Decimal
-  out_Animal_BornAt: [BirthEvent]
-  out_Animal_FedAt: [BirthEvent]
-  out_Animal_ImportantEvent: [Union__BirthEvent__Event]
-  out_Animal_LivesIn: [Location]
-  out_Animal_OfSpecies: [Species]
-  out_Animal_ParentOf: [Animal]
-  out_Entity_Related: [Entity]
-  uuid: ID
-}
-
-type BirthEvent implements Entity, UniquelyIdentifiable {
-  _x_count: Int
-  alias: [String]
-  description: String
-  event_date: DateTime
-  in_Animal_BornAt: [Animal]
-  in_Animal_FedAt: [Animal]
-  in_Animal_ImportantEvent: [Animal]
-  in_Entity_Related: [Entity]
-  in_Event_RelatedEvent: [Union__BirthEvent__Event]
-  name: String
-  out_Entity_Related: [Entity]
-  out_Event_RelatedEvent: [Union__BirthEvent__Event]
-  uuid: ID
-}
-
-
-interface UniquelyIdentifiable {
-  _x_count: Int
-  uuid: ID
-}
-
-
-scalar Date
-
-scalar DateTime
-
-scalar Decimal
-
-        interface Entity {
-            _x_count: Int
-            alias: [String]
-            description: String
-            in_Entity_Related: [Entity]
-            name: String
-            out_Entity_Related: [Entity]
-            uuid: ID
-        }
-        
-        type Event implements Entity {
-            _x_count: Int
-            alias: [String]
-            description: String
-            event_date: DateTime
-            in_Animal_ImportantEvent: [Animal]
-            in_Entity_Related: [Entity]
-            in_Event_RelatedEvent: [Union__BirthEvent__Event]
-            name: String
-            out_Entity_Related: [Entity]
-            out_Event_RelatedEvent: [Union__BirthEvent__Event]
-            uuid: ID
-        }
-        
-type Food implements Entity, UniquelyIdentifiable {
-  _x_count: Int
-  alias: [String]
-  description: String
-  in_Entity_Related: [Entity]
-  in_Species_Eats: [Species]
-  name: String
-  out_Entity_Related: [Entity]
-  uuid: ID
-}
-
-type FoodOrSpecies implements Entity, UniquelyIdentifiable {
-  _x_count: Int
-  alias: [String]
-  description: String
-  in_Entity_Related: [Entity]
-  in_Species_Eats: [Species]
-  name: String
-  out_Entity_Related: [Entity]
-  uuid: ID
-}
-
-type Location implements Entity, UniquelyIdentifiable {
-  _x_count: Int
-  alias: [String]
-  description: String
-  in_Animal_LivesIn: [Animal]
-  in_Entity_Related: [Entity]
-  name: String
-  out_Entity_Related: [Entity]
-  uuid: ID
-}
-        
-        type RootSchemaQuery {
-            Animal: Animal
-            BirthEvent: BirthEvent
-            Entity: Entity
-            Event: Event
-            Food: Food
-            Location: Location
-            Species: Species
-            _x_count: Int
-        }
-        
-        type Species implements Entity {
-            _x_count: Int
-            alias: [String]
-            description: String
-            in_Animal_OfSpecies: [Animal]
-            in_Entity_Related: [Entity]
-            in_Species_Eats: [Species]
-            limbs: Int
-            name: String
-            out_Entity_Related: [Entity]
-            out_Species_Eats: [Union__Food__FoodOrSpecies__Species]
-            uuid: ID
-        }
-
-        union Union__Food__FoodOrSpecies__Species = Food | Species
-
-
-        # Because of the above, the base type for this union is Event.
-        union Union__BirthEvent__Event = Event | BirthEvent
-
-        
-        type Location {
-            _x_count: Int
-            in_Animal_LivesIn: [Animal]
-            name: String
-            uuid: ID
-        }
+    schema {
+      query: RootSchemaQuery
+    }
+    
+    directive @filter(op_name: String!, value: [String!]!) on FIELD | INLINE_FRAGMENT
+    
+    directive @tag(tag_name: String!) on FIELD
+    
+    directive @output(out_name: String!) on FIELD
+    
+    directive @output_source on FIELD
+    
+    directive @optional on FIELD
+    
+    directive @recurse(depth: Int!) on FIELD
+    
+    directive @fold on FIELD
+    
+    type Animal implements Entity, UniquelyIdentifiable {
+      _x_count: Int
+      alias: [String]
+      birthday: Date
+      color: String
+      description: String
+      in_Animal_ParentOf: [Animal]
+      in_Entity_Related: [Entity]
+      name: String
+      net_worth: Decimal
+      out_Animal_BornAt: [BirthEvent]
+      out_Animal_FedAt: [BirthEvent]
+      out_Animal_ImportantEvent: [Union__BirthEvent__Event]
+      out_Animal_LivesIn: [Location]
+      out_Animal_OfSpecies: [Species]
+      out_Animal_ParentOf: [Animal]
+      out_Entity_Related: [Entity]
+      uuid: ID
+    }
+    
+    type BirthEvent implements Entity, UniquelyIdentifiable {
+      _x_count: Int
+      alias: [String]
+      description: String
+      event_date: DateTime
+      in_Animal_BornAt: [Animal]
+      in_Animal_FedAt: [Animal]
+      in_Animal_ImportantEvent: [Animal]
+      in_Entity_Related: [Entity]
+      in_Event_RelatedEvent: [Union__BirthEvent__Event]
+      name: String
+      out_Entity_Related: [Entity]
+      out_Event_RelatedEvent: [Union__BirthEvent__Event]
+      uuid: ID
+    }
+    
+    scalar Date
+    
+    scalar DateTime
+    
+    scalar Decimal
+    
+    interface Entity {
+      _x_count: Int
+      alias: [String]
+      description: String
+      in_Entity_Related: [Entity]
+      name: String
+      out_Entity_Related: [Entity]
+      uuid: ID
+    }
+    
+    type Event implements Entity, UniquelyIdentifiable {
+      _x_count: Int
+      alias: [String]
+      description: String
+      event_date: DateTime
+      in_Animal_ImportantEvent: [Animal]
+      in_Entity_Related: [Entity]
+      in_Event_RelatedEvent: [Union__BirthEvent__Event]
+      name: String
+      out_Entity_Related: [Entity]
+      out_Event_RelatedEvent: [Union__BirthEvent__Event]
+      uuid: ID
+    }
+    
+    type Food implements Entity, UniquelyIdentifiable {
+      _x_count: Int
+      alias: [String]
+      description: String
+      in_Entity_Related: [Entity]
+      in_Species_Eats: [Species]
+      name: String
+      out_Entity_Related: [Entity]
+      uuid: ID
+    }
+    
+    type FoodOrSpecies implements Entity, UniquelyIdentifiable {
+      _x_count: Int
+      alias: [String]
+      description: String
+      in_Entity_Related: [Entity]
+      in_Species_Eats: [Species]
+      name: String
+      out_Entity_Related: [Entity]
+      uuid: ID
+    }
+    
+    type Location implements Entity, UniquelyIdentifiable {
+      _x_count: Int
+      alias: [String]
+      description: String
+      in_Animal_LivesIn: [Animal]
+      in_Entity_Related: [Entity]
+      name: String
+      out_Entity_Related: [Entity]
+      uuid: ID
+    }
+    
+    type RootSchemaQuery {
+      Animal: Animal
+      BirthEvent: BirthEvent
+      Entity: Entity
+      Event: Event
+      Food: Food
+      FoodOrSpecies: FoodOrSpecies
+      Location: Location
+      Species: Species
+      UniquelyIdentifiable: UniquelyIdentifiable
+    }
+    
+    type Species implements Entity, UniquelyIdentifiable {
+      _x_count: Int
+      alias: [String]
+      description: String
+      in_Animal_OfSpecies: [Animal]
+      in_Entity_Related: [Entity]
+      in_Species_Eats: [Species]
+      limbs: Int
+      name: String
+      out_Entity_Related: [Entity]
+      out_Species_Eats: [Union__Food__FoodOrSpecies__Species]
+      uuid: ID
+    }
+    
+    union Union__BirthEvent__Event = BirthEvent | Event
+    
+    union Union__Food__FoodOrSpecies__Species = Food | FoodOrSpecies | Species
+    
+    interface UniquelyIdentifiable {
+      _x_count: Int
+      uuid: ID
+    }
     '''
 
     ast = parse(schema_text)
