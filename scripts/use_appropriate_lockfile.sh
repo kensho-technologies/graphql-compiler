5a5f816fa7b63ad42188c0eba50bccbc89555952
Predrag Gruevski <predrag@kensho.com>
2019-03-20 19:09:31 -0400

    Fix for python 2 printing version info to stderr instead of stdout.

--- scripts/use_appropriate_lockfile.sh
+++ scripts/use_appropriate_lockfile.sh
@@ -11,12 +11,12 @@ shopt -s globstar nullglob
 # Make sure the current working directory for this script is the root directory.
 cd "$(git -C "$(dirname "${0}")" rev-parse --show-toplevel )"
 
-is_py2="$(python --version | grep 'Python 2')"
+is_py2="$(python --version 2>&1 | grep 'Python 2')"  # N.B.: Python 2 prints version info to stderr.
 
 if [[ "$is_py2" != '' ]]; then
     echo "Found $is_py2, switching lockfiles to use the one compatible with Python 2."
     mv Pipfile.lock Pipfile.py3.lock
     mv Pipfile.py2.lock Pipfile.lock
 else
-    echo "Python was $(python --version), sticking with Python 3 lockfile."
+    echo "Python was $(python --version 2>&1), sticking with Python 3 lockfile."
 fi
