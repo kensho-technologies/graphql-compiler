8acc64e3a57bcb0065f2e601233dcc019380bfb3
Predrag Gruevski <predrag@kensho.com>
2019-05-17 10:03:51 -0400

    Actually save all the files.

--- graphql_compiler/compiler/compiler_frontend.py
+++ graphql_compiler/compiler/compiler_frontend.py
@@ -85,9 +85,9 @@ from .directive_helpers import (
 )
 from .filters import process_filter_directive
 from .helpers import (
-    FoldScopeLocation, Location, get_ast_field_name, get_directive_argument_name,
+    FoldScopeLocation, Location, get_ast_field_name, get_parameter_name,
     get_edge_direction_and_name, get_field_type_from_schema, get_uniquely_named_objects_by_name,
-    get_vertex_field_type, invert_dict, is_tag_argument, is_vertex_field_name,
+    get_vertex_field_type, invert_dict, is_tagged_parameter, is_vertex_field_name,
     strip_non_null_from_type, validate_output_name, validate_safe_string
 )
 from .metadata import LocationInfo, QueryMetadataTable, RecurseInfo, TagInfo
@@ -727,8 +727,8 @@ def _validate_all_tags_are_used(metadata):
     for location, _ in metadata.registered_locations:
         for filter_info in metadata.get_filter_infos(location):
             for filter_arg in filter_info.args:
-                if is_tag_argument(filter_arg):
-                    filter_arg_names.add(get_directive_argument_name(filter_arg))
+                if is_tagged_parameter(filter_arg):
+                    filter_arg_names.add(get_parameter_name(filter_arg))
 
     unused_tags = tag_names - filter_arg_names
     if unused_tags:
