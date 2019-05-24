5b64350f861c755bc125f4e453473a8a855bb3a9
Evan Tey <evan.tey@kensho.com>
2019-05-24 17:43:20 -0400

    Refactor cardinality estimation

--- graphql_compiler/cost_estimation/cardinality_estimator.py
+++ graphql_compiler/cost_estimation/cardinality_estimator.py
@@ -9,6 +9,27 @@ from ..schema_generation.orientdb.schema_properties import (
 from .filter_selectivity_utils import adjust_counts_for_filters
 
 
+def _is_subexpansion_optional(query_metadata, child_location, parent_location):
+    """Return True if child_location is the root of an optional subexpansion."""
+    child_optional_depth = query_metadata.get_location_info(child_location).optional_scopes_depth
+    parent_optional_depth = query_metadata.get_location_info(parent_location).optional_scopes_depth
+    return child_optional_depth > parent_optional_depth
+
+
+def _is_subexpansion_folded(location):
+    """Return True if location is the root of a folded subexpansion."""
+    return isinstance(location, FoldScopeLocation) and len(location.fold_path) == 1
+
+
+def _is_subexpansion_recursive(query_metadata, child_location, parent_location):
+    """Return True if child_location is the root of a recursive subexpansion."""
+    edge_direction, edge_name = _get_last_edge_direction_and_name_to_location(child_location)
+    for recurse_info in query_metadata.get_recurse_infos(parent_location):
+        if recurse_info.edge_direction == edge_direction and recurse_info.edge_name == edge_name:
+            return True
+    return False
+
+
 def _get_all_original_child_locations(query_metadata, start_location):
     """Get all original child Locations of a start Location and revisits to the start Location.
 
@@ -47,13 +68,26 @@ def _get_last_edge_direction_and_name_to_location(location):
     return edge_direction, edge_name
 
 
-def _estimate_child_edges_per_parent(
-    schema_graph, lookup_class_counts, query_metadata, child_location
+def _get_parent_and_child_name_from_edge(schema_graph, child_location):
+    """Get the base classname of a location and its parent from the last edge information."""
+    edge_direction, edge_name = _get_last_edge_direction_and_name_to_location(child_location)
+    edge_element = schema_graph.get_edge_schema_element_or_raise(edge_name)
+    if edge_direction == EDGE_DESTINATION_PROPERTY_NAME:
+        parent_name_from_edge = edge_element.base_out_connection
+        child_name_from_edge = edge_element.base_in_connection
+    elif edge_direction == EDGE_SOURCE_PROPERTY_NAME:
+        parent_name_from_edge = edge_element.base_in_connection
+        child_name_from_edge = edge_element.base_out_connection
+    return parent_name_from_edge, child_name_from_edge
+
+
+def _estimate_children_per_parent(
+    schema_graph, lookup_class_counts, query_metadata, parameters, child_location, parent_location
 ):
     """Estimate the number of edges per parent_location that connect to child_location vertices.
 
-    Given a parent location of type A and child location of type B, assume that all AB edges are
-    distributed evenly on A vertices, so the expected number of child edges per parent vertex is
+    Given a parent location of type A and child location of type B, assume all AB edges are
+    distributed evenly over A vertices, so the expected number of child edges per parent vertex is
     (# of AB edges) / (# of A vertices). If A and B are subclasses of C and D respectively, we only
     have access to CD edges, so in general, we'll use (# of CD edges) / (# of C vertices), but since
     we're only interested in edges to Bs, we'll scale this result by the fraction of Bs over Ds.
@@ -63,25 +97,21 @@ def _estimate_child_edges_per_parent(
         lookup_class_counts: function, string -> int, that accepts a class name and returns the
                              total number of instances plus subclass instances
         query_metadata: QueryMetadataTable object
+        parameters: dict, parameters with which query will be executed
         child_location: BaseLocation object
 
     Returns:
         float, expected number of child_location vertices connected to each parent_location vertex.
     """
-    # Get direction and name of edge between parent and child location and its base endpoint names.
-    edge_direction, edge_name = _get_last_edge_direction_and_name_to_location(child_location)
-    edge_element = schema_graph.get_edge_schema_element_or_raise(edge_name)
-    if edge_direction == EDGE_DESTINATION_PROPERTY_NAME:
-        parent_name_from_edge = edge_element.base_out_connection
-        child_name_from_edge = edge_element.base_in_connection
-    elif edge_direction == EDGE_SOURCE_PROPERTY_NAME:
-        parent_name_from_edge = edge_element.base_in_connection
-        child_name_from_edge = edge_element.base_out_connection
-
+    # Count the number of edges between child_location and parent_location type vertices.
+    _, edge_name = _get_last_edge_direction_and_name_to_location(child_location)
     # TODO(evan): If edge is recursed over, we need a more detailed statistic
     edge_counts = lookup_class_counts(edge_name)
 
     # Scale edge_counts if child_location's type is a subclass of the edge's endpoint type.
+    parent_name_from_edge, child_name_from_edge = _get_parent_and_child_name_from_edge(
+        schema_graph, child_location
+    )
     child_name_from_location = query_metadata.get_location_info(child_location).type.name
     if child_name_from_edge != child_name_from_location:
         edge_counts *= (
@@ -89,39 +119,32 @@ def _estimate_child_edges_per_parent(
             lookup_class_counts(child_name_from_edge)
         )
 
+    # Count the number of parents, over which we assume the edges are uniformly distributed.
     parent_counts = lookup_class_counts(parent_name_from_edge)
 
     # TODO(evan): edges are not necessarily uniformly distributed, so record more statistics
-    child_edges_per_parent = float(edge_counts) / parent_counts
+    child_counts_per_parent = float(edge_counts) / parent_counts
 
-    return child_edges_per_parent
+    # Recursion always starts with depth = 0, so we should treat the parent result set itself as a
+    # child result set to be expanded (so add 1 to child_counts).
+    is_recursive = _is_subexpansion_recursive(query_metadata, child_location, parent_location)
+    if is_recursive:
+        child_counts_per_parent += 1
 
+    # Adjust the counts for filters at child_location.
+    child_filters = query_metadata.get_filter_infos(child_location)
+    child_counts_per_parent = adjust_counts_for_filters(
+        schema_graph, lookup_class_counts, child_filters, parameters, child_name_from_location,
+        child_counts_per_parent
+    )
 
-def _is_subexpansion_optional(query_metadata, child_location, parent_location):
-    """Return True if child_location is the root of an optional subexpansion."""
-    child_optional_depth = query_metadata.get_location_info(child_location).optional_scopes_depth
-    parent_optional_depth = query_metadata.get_location_info(parent_location).optional_scopes_depth
-    return child_optional_depth > parent_optional_depth
-
-
-def _is_subexpansion_folded(location):
-    """Return True if location is the root of a folded subexpansion."""
-    return isinstance(location, FoldScopeLocation) and len(location.fold_path) == 1
-
-
-def _is_subexpansion_recursive(query_metadata, child_location, parent_location):
-    """Return True if child_location is the root of a recursive subexpansion."""
-    edge_direction, edge_name = _get_last_edge_direction_and_name_to_location(child_location)
-    for recurse_info in query_metadata.get_recurse_infos(parent_location):
-        if recurse_info.edge_direction == edge_direction and recurse_info.edge_name == edge_name:
-            return True
-    return False
+    return child_counts_per_parent
 
 
-def _estimate_subexpansion_results(
+def _estimate_subexpansion_cardinality(
     schema_graph, lookup_class_counts, query_metadata, parameters, child_location, parent_location
 ):
-    """Estimate the number of result sets in the subexpansion associated with child_location.
+    """Estimate the cardinality associated with the subexpansion of a child_location vertex.
 
     Args:
         schema_graph: SchemaGraph object
@@ -131,8 +154,7 @@ def _estimate_subexpansion_results(
         parameters: dict, parameters with which query will be executed
         child_location: BaseLocation object, child of parent_location corresponding to the
                         subexpansion root
-        parent_location: BaseLocation object, location corresponding to the type of vertex being
-                         expanded
+        parent_location: BaseLocation object, location corresponding to the vertex being expanded
 
     Returns:
         float, number of expected result sets found when a vertex corresponding to parent_location
@@ -142,31 +164,16 @@ def _estimate_subexpansion_results(
         estimate this recursively as:
         (expected number of B-vertices) * (expected number of result sets per B-vertex).
     """
-    child_counts = _estimate_child_edges_per_parent(
-        schema_graph, lookup_class_counts, query_metadata, child_location
+    child_counts_per_parent = _estimate_children_per_parent(
+        schema_graph, lookup_class_counts, query_metadata, parameters, child_location,
+        parent_location
     )
 
-    # Recursion always starts with depth = 0, so we should treat the parent result set itself as a
-    # child result set to be expanded (so add 1 to child_counts).
-    is_recursive = _is_subexpansion_recursive(query_metadata, child_location, parent_location)
-    if is_recursive:
-        child_counts += 1
-
-    child_counts = adjust_counts_for_filters(
-        schema_graph, lookup_class_counts, query_metadata.get_filter_infos(child_location),
-        parameters, query_metadata.get_location_info(child_location).type.name, child_counts
+    results_per_child = _estimate_expansion_cardinality(
+        schema_graph, lookup_class_counts, query_metadata, parameters, child_location
     )
 
-    results_per_child = 1
-    grandchild_locations = _get_all_original_child_locations(query_metadata, child_location)
-    for grandchild_location in grandchild_locations:
-        subsubexpansion_results = _estimate_subexpansion_results(
-            schema_graph, lookup_class_counts, query_metadata, parameters, grandchild_location,
-            child_location
-        )
-        results_per_child *= subsubexpansion_results
-
-    subexpansion_results = child_counts * results_per_child
+    subexpansion_cardinality = child_counts_per_parent * results_per_child
 
     # If child_location is the root of an optional or folded subexpansion, the empty result set will
     # be returned if no other result sets exist, so return at least 1.
@@ -174,9 +181,39 @@ def _estimate_subexpansion_results(
     is_optional = _is_subexpansion_optional(query_metadata, child_location, parent_location)
     is_folded = _is_subexpansion_folded(child_location)
     if is_optional or is_folded:
-        subexpansion_results = max(subexpansion_results, 1)
+        subexpansion_cardinality = max(subexpansion_cardinality, 1)
 
-    return subexpansion_results
+    return subexpansion_cardinality
+
+
+def _estimate_expansion_cardinality(
+    schema_graph, lookup_class_counts, query_metadata, parameters, current_location
+):
+    """Estimate the cardinality of fully expanding a vertex corresponding to current_location.
+
+    Args:
+        schema_graph: SchemaGraph object
+        lookup_class_counts: function, string -> int, that accepts a class name and returns the
+                             total number of instances plus subclass instances
+        query_metadata: QueryMetadataTable object
+        parameters: dict, parameters with which query will be executed
+        current_location: BaseLocation object, corresponding to the vertex we're expanding
+
+    Returns:
+        float, expected cardinality associated with the full expansion of one current vertex.
+    """
+    expansion_cardinality = 1
+    child_locations = _get_all_original_child_locations(query_metadata, current_location)
+    for child_location in child_locations:
+        # The expected cardinality per current vertex is the product of the expected cardinality for
+        # each subexpansion (e.g. If we expect each current vertex to have 2 children of type A and
+        # 3 children of type B, we'll return 6 distinct result sets per current vertex).
+        subexpansion_cardinality = _estimate_subexpansion_cardinality(
+            schema_graph, lookup_class_counts, query_metadata, parameters, child_location,
+            current_location
+        )
+        expansion_cardinality *= subexpansion_cardinality
+    return expansion_cardinality
 
 
 def estimate_query_result_cardinality(
@@ -225,17 +262,9 @@ def estimate_query_result_cardinality(
     )
 
     # Next, find the number of expected result sets per root vertex when fully expanded
-    results_per_root = 1
-    child_locations = _get_all_original_child_locations(query_metadata, root_location)
-    for child_location in child_locations:
-        # The number of expected result sets found per root vertex is the product of the expected
-        # result sets in each subexpansion (e.g. If we expect each root vertex to have 2 children of
-        # type A and 3 children of type B, we'll return 6 distinct result sets per root vertex).
-        subexpansion_results = _estimate_subexpansion_results(
-            schema_graph, lookup_class_counts, query_metadata, parameters, child_location,
-            root_location
-        )
-        results_per_root *= subexpansion_results
+    results_per_root = _estimate_expansion_cardinality(
+        schema_graph, lookup_class_counts, query_metadata, parameters, root_location
+    )
 
     expected_query_result_cardinality = root_counts * results_per_root
 
