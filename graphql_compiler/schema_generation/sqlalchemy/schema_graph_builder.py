826645a2e318077b5f89f6305fb733ed5cb24eb6
Pedro Mantica <pedro.mantica@kensho.com>
2019-06-03 13:02:53 -0400

    Add MSSQL types

--- graphql_compiler/schema_generation/sqlalchemy/schema_graph_builder.py
+++ graphql_compiler/schema_generation/sqlalchemy/schema_graph_builder.py
@@ -1,68 +1,9 @@
 # Copyright 2019-present Kensho Technologies, LLC.
-import warnings
-
-from graphql.type import GraphQLBoolean, GraphQLFloat, GraphQLString
 import six
 from sqlalchemy.schema import ColumnDefault
-import sqlalchemy.sql.sqltypes as sqltypes
 
-from ...schema import GraphQLDate, GraphQLDateTime, GraphQLDecimal, GraphQLInt
 from ..schema_graph import InheritanceStructure, PropertyDescriptor, SchemaGraph, VertexType
-
-
-# TODO(pmantica1): Add scalar mapping for the following classes: Interval.
-# The following quote from https://docs.sqlalchemy.org/en/13/core/type_basics.html
-# explains what makes the all-cap classes particular:
-# "This category of types refers to types that are either part of the SQL standard, or are
-# potentially found within a subset of database backends. Unlike the "generic" types, the SQL
-# standard/multi-vendor types have no guarantee of working on all backends, and will only work
-# on those backends that explicitly support them by name. That is, the type will always emit its
-# exact name in DDL with CREATE TABLE is issued."
-SQL_CLASS_TO_GRAPHQL_TYPE = {
-    sqltypes.BIGINT: GraphQLInt,
-    sqltypes.BigInteger: GraphQLInt,
-    sqltypes.Boolean: GraphQLBoolean,
-    sqltypes.CHAR: GraphQLString,
-    sqltypes.CLOB: GraphQLString,
-    sqltypes.Date: GraphQLDate,
-    sqltypes.DATE: GraphQLDate,
-    sqltypes.DateTime: GraphQLDateTime,
-    sqltypes.DATETIME: GraphQLDateTime,
-    sqltypes.Enum: GraphQLString,
-    sqltypes.Float: GraphQLFloat,
-    sqltypes.FLOAT: GraphQLFloat,
-    sqltypes.INT: GraphQLInt,
-    sqltypes.Integer: GraphQLInt,
-    sqltypes.INTEGER: GraphQLInt,
-    sqltypes.NCHAR: GraphQLString,
-    sqltypes.Numeric: GraphQLDecimal,
-    sqltypes.NUMERIC: GraphQLDecimal,
-    sqltypes.NVARCHAR: GraphQLString,
-    sqltypes.REAL: GraphQLFloat,
-    sqltypes.SMALLINT: GraphQLInt,
-    sqltypes.SmallInteger: GraphQLInt,
-    sqltypes.String: GraphQLString,
-    sqltypes.Text: GraphQLString,
-    sqltypes.TEXT: GraphQLString,
-    sqltypes.Time: GraphQLDateTime,
-    sqltypes.TIME: GraphQLDateTime,
-    sqltypes.TIMESTAMP: GraphQLDateTime,
-    sqltypes.Unicode: GraphQLString,
-    sqltypes.UnicodeText: GraphQLString,
-    sqltypes.VARCHAR: GraphQLString,
-}
-
-# We do not currently plan to add a mapping for JSON and Binary objects.
-UNSUPPORTED_PRIMITIVE_TYPES = frozenset({
-    sqltypes.ARRAY,
-    sqltypes.Binary,
-    sqltypes.BINARY,
-    sqltypes.Interval,
-    sqltypes.JSON,
-    sqltypes.LargeBinary,
-    sqltypes.PickleType,
-    sqltypes.VARBINARY,
-})
+from ..sqlalchemy.scalar_type_mapper import try_get_graphql_scalar_type
 
 
 # TODO(pmantica1): Map foreign keys to edges.
@@ -78,17 +19,6 @@ def get_schema_graph_from_sql_alchemy_metadata(sqlalchemy_metadata):
     return SchemaGraph(elements, InheritanceStructure(direct_superclass_sets), set())
 
 
-def _try_get_graphql_scalar_type(column_name, column_type):
-    """Return the matching GraphQLScalarType for the SQL datatype or None if none is found."""
-    maybe_graphql_type = SQL_CLASS_TO_GRAPHQL_TYPE.get(type(column_type), None)
-    if maybe_graphql_type is None:
-        # Trying to get the string representation of the SQLAlchemy JSON and ARRAY types
-        # will lead to an error. We therefore use repr instead.
-        warnings.warn(u'Ignoring column "{}" with unsupported SQL datatype class: {}'
-                      .format(column_name, repr(column_type)))
-    return maybe_graphql_type
-
-
 # TODO(pmantica1): Address nullable types.
 # TODO(pmantica1): Map Enum to the GraphQL Enum type.
 # TODO(pmantica1): Map arrays to GraphQLLists once the compiler is able to handle them.
@@ -98,7 +28,7 @@ def _get_vertex_type_from_sqlalchemy_table(table):
     properties = dict()
     for column in table.get_children():
         name = column.key
-        maybe_property_type = _try_get_graphql_scalar_type(name, column.type)
+        maybe_property_type = try_get_graphql_scalar_type(name, column.type)
         if maybe_property_type is not None:
             default = None
             # TODO(pmantica1): Parse Sequence type default values.
--- graphql_compiler/tests/test_sqlalchemy_schema_generation.py
+++ graphql_compiler/tests/test_sqlalchemy_schema_generation.py
@@ -1,14 +1,16 @@
 # Copyright 2019-present Kensho Technologies, LLC.
 import unittest
 
-from graphql.type import GraphQLString
+from graphql.type import GraphQLInt, GraphQLString
 import pytest
 from sqlalchemy import Column, MetaData, Table
+from sqlalchemy.dialects.mssql.base import TINYINT
 from sqlalchemy.types import Binary, Integer, String
 
 from ..schema_generation.schema_graph import VertexType
+from ..schema_generation.sqlalchemy.scalar_type_mapper import try_get_graphql_scalar_type
 from ..schema_generation.sqlalchemy.schema_graph_builder import (
-    _try_get_graphql_scalar_type, get_schema_graph_from_sql_alchemy_metadata
+    get_schema_graph_from_sql_alchemy_metadata
 )
 
 
@@ -21,7 +23,8 @@ def _get_sql_metadata():
         metadata,
         Column('supported_type', String()),
         Column('non_supported_type', Binary()),
-        Column('default', Integer(), default=42)
+        Column('default', Integer(), default=42),
+        Column('column_with_mssql_type', TINYINT),
     )
 
     return metadata
@@ -45,7 +48,7 @@ class SQLALchemyGraphqlSchemaGenerationTests(unittest.TestCase):
 
     def test_warn_when_type_is_not_supported(self):
         with pytest.warns(Warning):
-            _try_get_graphql_scalar_type('binary', Binary)
+            try_get_graphql_scalar_type('binary', Binary)
 
     def test_default_value_parsing(self):
         a_vertex = self.schema_graph.get_element_by_class_name('A')
@@ -55,3 +58,7 @@ class SQLALchemyGraphqlSchemaGenerationTests(unittest.TestCase):
         # Tests that a class is in its own subclass/superclass set.
         self.assertEqual(self.schema_graph.get_subclass_set('A'), {'A'})
         self.assertEqual(self.schema_graph.get_superclass_set('A'), {'A'})
+
+    def test_mssql_type_mapping(self):
+        a_vertex = self.schema_graph.get_element_by_class_name('A')
+        self.assertEqual(a_vertex.properties['column_with_mssql_type'], GraphQLInt)
