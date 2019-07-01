0a86b8149fbe876932562d1d08281c03569926fa
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2019-07-01 17:17:17 -0400

    Remove unused statistics

--- graphql_compiler/cost_estimation/statistics.py
+++ graphql_compiler/cost_estimation/statistics.py
@@ -60,50 +60,10 @@ class Statistics(object):
         """
         return None
 
-    @abstractmethod
-    def get_distinct_field_values_count(self, vertex_name, field_name):
-        """Return the count of distinct values the given vertex field has over all vertex instances.
-
-        This statistic helps estimate the result size of @filter directives used to restrict
-        values using equality operators like '=', '!=', and 'in_collection'.
-
-        Args:
-            vertex_name: str, name of a vertex.
-            field_name: str, name of a vertex field.
-
-        Returns:
-            - int, the count of distinct values of that vertex field if the statistic exists
-            - None otherwise
-        """
-        return None
-
-    @abstractmethod
-    def get_histogram(self, vertex_name, field_name):
-        """Return a histogram for the given vertex field, providing statistics about range values.
-
-        This statistic helps estimate the result size of @filter directives used to restrict
-        numbers using inequality operators like '>', '<', and 'between'.
-
-        Args:
-            vertex_name: str, name of a vertex.
-            field_name: str, name of a vertex field.
-
-        Returns:
-            - list[tuple(float, float, int)], histogram as a list of tuples with 3 elements each if
-              the statistic exists. Each tuple represents a bin as (start, end, elementCount), where
-              [start, end) is the range of values in the bin, and elementCount is the bin element
-              count.
-            - None otherwise
-        """
-        return None
-
 
 class LocalStatistics(Statistics):
     """Provides statistics using ones given at initialization."""
-    def __init__(
-        self, class_counts, edge_count_between_vertex_pairs=None,
-        count_of_distinct_field_values=None, histograms=None
-    ):
+    def __init__(self, class_counts, *, edge_count_between_vertex_pairs=None):
         """Initializes statistics with the given data.
 
         Args:
@@ -111,23 +71,12 @@ class LocalStatistics(Statistics):
             edge_count_between_vertex_pairs: optional dict, (str, str, str) -> int, mapping
                 tuple of (vertex source class name, edge class name, vertex target class name) to
                 count of edge instances of given class connecting instances of two vertex classes.
-            count_of_distinct_values: optional dict, (str, str) -> int, mapping vertex class name
-                and field name on that vertex class to the count of distinct values of the field for
-                that vertex class.
-            histograms: optional dict, (str, str) -> list[tuple(float, float, int)], mapping vertex
-                class name and field name on that vertex class to histogram.
         """
         if edge_count_between_vertex_pairs is None:
             edge_count_between_vertex_pairs = dict()
-        if count_of_distinct_field_values is None:
-            count_of_distinct_field_values = dict()
-        if histograms is None:
-            histograms = dict()
 
         self._class_counts = class_counts
         self._edge_count_between_vertex_pairs = edge_count_between_vertex_pairs
-        self._count_of_distinct_field_values = count_of_distinct_field_values
-        self._histograms = histograms
 
     def get_class_count(self, class_name):
         """Return how many vertex or edge instances have, or inherit, the given class name.
@@ -168,40 +117,3 @@ class LocalStatistics(Statistics):
         """
         statistic_key = (vertex_source_class_name, edge_class_name, vertex_target_class_name)
         return self._edge_count_between_vertex_pairs.get(statistic_key)
-
-    def get_distinct_field_values_count(self, vertex_name, field_name):
-        """Return the count of distinct values the given vertex field has over all vertex instances.
-
-        This statistic helps estimate the result size of @filter directives used to restrict
-        values using equality operators like '=', '!=', and 'in_collection'.
-
-        Args:
-            vertex_name: str, name of a vertex.
-            field_name: str, name of a vertex field.
-
-        Returns:
-            - int, the count of distinct values of that vertex field if the statistic exists
-            - None otherwise
-        """
-        statistic_key = (vertex_name, field_name)
-        return self._count_of_distinct_field_values.get(statistic_key)
-
-    def get_histogram(self, vertex_name, field_name):
-        """Return a histogram for the given vertex field, providing statistics about range values.
-
-        This statistic helps estimate the result size of @filter directives used to restrict
-        numbers using inequality operators like '>', '<', and 'between'.
-
-        Args:
-            vertex_name: str, name of a vertex.
-            field_name: str, name of a vertex field.
-
-        Returns:
-            - list[tuple(float, float, int)], histogram as a list of tuples with 3 elements each if
-              the statistic exists. Each tuple represents a bin as (start, end, elementCount), where
-              [start, end) is the range of values in the bin, and elementCount is the bin element
-              count.
-            - None otherwise
-        """
-        histogram_key = (vertex_name, field_name)
-        return self._histograms.get(histogram_key)
