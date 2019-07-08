3c3726a8ad5d4a3258e4978ec296dc1f966d3d92
Pedro Mantica <pedro.mantica@kensho.com>
2019-07-08 18:31:06 -0400

    Remove disabling of pylint messages

--- graphql_compiler/tests/test_helpers.py
+++ graphql_compiler/tests/test_helpers.py
@@ -278,18 +278,16 @@ def get_schema():
     schema = build_ast_schema(ast)
     # The schema text contains no information about how to parse or serialize non-builtin scalar
     # types so we add this information manually. For more info see the link below:
-    # pylint: disable=max-line-length, protected-access
-    # https://stackoverflow.com/questions/47824603/graphql-custom-scalar-definition-without-graphql-tools
     for graphql_type in CUSTOM_SCALAR_TYPES:
         # We cannot simply replace the value corresponding to the key graphql_type.name.
         # We actually need to modify the value because it is referenced in other places in the
         # schema such as graphql object fields.
-        if graphql_type.name in schema._type_map:
-            schema._type_map[graphql_type.name].description = graphql_type.description
-            schema._type_map[graphql_type.name].serialize = graphql_type.serialize
-            schema._type_map[graphql_type.name].parse_value = graphql_type.parse_value
-            schema._type_map[graphql_type.name].parse_literal = graphql_type.parse_literal
-    # pylint: enable=max-line-length, protected-access
+        matching_schema_type = schema.get_type(graphql_type.name)
+        if matching_schema_type:
+            matching_schema_type.description = graphql_type.description
+            matching_schema_type.serialize = graphql_type.serialize
+            matching_schema_type.parse_value = graphql_type.parse_value
+            matching_schema_type.parse_literal = graphql_type.parse_literal
     return schema
 
 
