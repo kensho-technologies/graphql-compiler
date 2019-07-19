ce3f4ab67f12c94b00ebd93aee5a3defcd851a8c
Pedro Mantica <pedro.mantica@kensho.com>
2019-07-19 13:34:42 -0400

    Improve validation error message.

--- graphql_compiler/compiler/helpers.py
+++ graphql_compiler/compiler/helpers.py
@@ -161,7 +161,10 @@ def validate_safe_string(value):
 
     if not set(value).issubset(VARIABLE_ALLOWED_CHARS) and \
             value not in legal_strings_with_special_chars:
-        raise GraphQLCompilationError(u'Encountered illegal characters in string: {}'.format(value))
+        raise GraphQLCompilationError(u'Encountered illegal characters in string: {}. '
+                                      u'Strings are only allowed to have underscores, '
+                                      u'digits and upper and lower case alphabetical letters.'
+                                      .format(value))
 
 
 def validate_output_name(value):
