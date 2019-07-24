2c1e5dca39be6ae1b6f3c3c0349416c49cbdba91
LWprogramming <LWprogramming@users.noreply.github.com>
2019-07-24 11:06:25 -0400

    Update MATCH emit output tests (#421)
    * Add GlobalOperationsStart block to each ir_blocks list
    
    Addresses comment from PR #379
    
    https://github.com/kensho-technologies/graphql-compiler/pull/379#discussion_r304515049
    
    * Fix imports
    
    * Adapt MATCH emit output tests to use test schema
    
    * Fix incorrect emit output test
    
    test_simple_traverse_filter_output() is supposed to comapre a child
    vertex's name with the parent's name, and just comparing to a variable
    named `desired_name` defeats the point of this test. EmitGremlinTests
    seems to do it right.
    
    * Nit
    
    * remove newline

--- graphql_compiler/tests/test_emit_output.py
+++ graphql_compiler/tests/test_emit_output.py
@@ -28,24 +28,29 @@ class EmitMatchTests(unittest.TestCase):
         self.maxDiff = None
 
     def test_simple_immediate_output(self):
-        base_location = Location(('Foo',))
+        # corresponds to:
+        # graphql_string = '''{
+        #     Animal {
+        #         name @output(out_name: "animal_name")
+        #     }
+        # }'''
+        base_location = Location(('Animal',))
         base_name_location = base_location.navigate_to_field('name')
 
         ir_blocks = [
-            QueryRoot({'Foo'}),
+            QueryRoot({'Animal'}),
             MarkLocation(base_location),
-            ConstructResult({
-                'foo_name': OutputContextField(base_name_location, GraphQLString),
-            }),
+            GlobalOperationsStart(),
+            ConstructResult({'animal_name': OutputContextField(base_name_location, GraphQLString)}),
         ]
         match_query = convert_to_match_query(ir_blocks)
         compound_match_query = CompoundMatchQuery(match_queries=[match_query])
 
         expected_match = '''
-            SELECT Foo___1.name AS `foo_name` FROM (
+            SELECT Animal___1.name AS `animal_name` FROM (
                 MATCH {{
-                    class: Foo,
-                    as: Foo___1
+                    class: Animal,
+                    as: Animal___1
                 }}
                 RETURN $matches
             )
@@ -55,40 +60,50 @@ class EmitMatchTests(unittest.TestCase):
         compare_match(self, expected_match, received_match)
 
     def test_simple_traverse_filter_output(self):
-        base_location = Location(('Foo',))
+        # corresponds to:
+        # graphql_string = '''{
+        #     Animal {
+        #         name @tag(tag_name: "name")
+        #              @output(out_name: "animal_name")
+        #         out_Animal_BornAt {
+        #             name @filter(op_name: "=", value: ["%name"])
+        #         }
+        #     }
+        # }'''
+        base_location = Location(('Animal',))
         base_name_location = base_location.navigate_to_field('name')
-        child_location = base_location.navigate_to_subpath('out_Foo_Bar')
+        child_location = base_location.navigate_to_subpath('out_Animal_BornAt')
 
         ir_blocks = [
-            QueryRoot({'Foo'}),
+            QueryRoot({'Animal'}),
+            MarkLocation(base_location),
+            Traverse('out', 'Animal_BornAt'),
             Filter(BinaryComposition(
                 u'=',
                 LocalField(u'name', GraphQLString),
-                Variable('$desired_name', GraphQLString))),
-            MarkLocation(base_location),
-            Traverse('out', 'Foo_Bar'),
+                ContextField(base_name_location, GraphQLString))),
             MarkLocation(child_location),
-
-            QueryRoot({'Foo'}),
+            QueryRoot({'Animal'}),
             MarkLocation(base_location),
+            GlobalOperationsStart(),
             ConstructResult({
-                'foo_name': OutputContextField(base_name_location, GraphQLString),
+                'animal_name': OutputContextField(base_name_location, GraphQLString),
             }),
         ]
         match_query = convert_to_match_query(ir_blocks)
         compound_match_query = CompoundMatchQuery(match_queries=[match_query])
 
         expected_match = '''
-            SELECT Foo___1.name AS `foo_name` FROM (
+            SELECT Animal___1.name AS `animal_name` FROM (
                 MATCH {{
-                    class: Foo,
-                    where: ((name = {desired_name})),
-                    as: Foo___1
-                }}.out('Foo_Bar') {{
-                    as: Foo__out_Foo_Bar___1
+                    class: Animal,
+                    as: Animal___1
+                }}.out('Animal_BornAt') {{
+                    where: ((name = $matched.Animal___1.name)),
+                    as: Animal__out_Animal_BornAt___1
                 }} , {{
-                    class: Foo,
-                    as: Foo___1
+                    class: Animal,
+                    as: Animal___1
                 }}
                 RETURN $matches
             )
@@ -98,11 +113,18 @@ class EmitMatchTests(unittest.TestCase):
         compare_match(self, expected_match, received_match)
 
     def test_datetime_variable_representation(self):
-        base_location = Location(('Event',))
+        # corresponds to:
+        # graphql_string = '''{
+        #     BirthEvent {
+        #         name @output(out_name: "name")
+        #         event_date @filter(op_name: "between", value: ["$start", "$end"])
+        #     }
+        # }'''
+        base_location = Location(('BirthEvent',))
         base_name_location = base_location.navigate_to_field('name')
 
         ir_blocks = [
-            QueryRoot({'Event'}),
+            QueryRoot({'BirthEvent'}),
             Filter(
                 BinaryComposition(
                     u'&&',
@@ -115,6 +137,7 @@ class EmitMatchTests(unittest.TestCase):
                 )
             ),
             MarkLocation(base_location),
+            GlobalOperationsStart(),
             ConstructResult({
                 'name': OutputContextField(base_name_location, GraphQLString)
             }),
@@ -123,14 +146,14 @@ class EmitMatchTests(unittest.TestCase):
         compound_match_query = CompoundMatchQuery(match_queries=[match_query])
 
         expected_match = '''
-            SELECT Event___1.name AS `name` FROM (
+            SELECT BirthEvent___1.name AS `name` FROM (
                 MATCH {{
-                    class: Event,
+                    class: BirthEvent,
                     where: ((
                         (event_date >= date({start}, "yyyy-MM-dd'T'HH:mm:ssX")) AND
                         (event_date <= date({end}, "yyyy-MM-dd'T'HH:mm:ssX"))
                     )),
-                    as: Event___1
+                    as: BirthEvent___1
                 }}
                 RETURN $matches
             )
@@ -140,24 +163,31 @@ class EmitMatchTests(unittest.TestCase):
         compare_match(self, expected_match, received_match)
 
     def test_datetime_output_representation(self):
-        base_location = Location(('Event',))
+        # corresponds to:
+        # graphql_string = '''{
+        #     BirthEvent {
+        #         event_date @output(out_name: "event_date")
+        #     }
+        # }'''
+        base_location = Location(('BirthEvent',))
         base_event_date_location = base_location.navigate_to_field('event_date')
 
         ir_blocks = [
-            QueryRoot({'Event'}),
+            QueryRoot({'BirthEvent'}),
             MarkLocation(base_location),
-            ConstructResult({
-                'event_date': OutputContextField(base_event_date_location, GraphQLDateTime)
-            }),
+            GlobalOperationsStart(),
+            ConstructResult(
+                {'event_date': OutputContextField(base_event_date_location, GraphQLDateTime)}
+            ),
         ]
         match_query = convert_to_match_query(ir_blocks)
         compound_match_query = CompoundMatchQuery(match_queries=[match_query])
 
         expected_match = '''
-            SELECT Event___1.event_date.format("yyyy-MM-dd'T'HH:mm:ssX") AS `event_date` FROM (
+            SELECT BirthEvent___1.event_date.format("yyyy-MM-dd'T'HH:mm:ssX") AS `event_date` FROM (
                 MATCH {{
-                    class: Event,
-                    as: Event___1
+                    class: BirthEvent,
+                    as: BirthEvent___1
                 }}
                 RETURN $matches
             )
@@ -179,6 +209,7 @@ class EmitGremlinTests(unittest.TestCase):
         ir_blocks = [
             QueryRoot({'Foo'}),
             MarkLocation(base_location),
+            GlobalOperationsStart(),
             ConstructResult({
                 'foo_name': OutputContextField(base_name_location, GraphQLString),
             })
@@ -210,6 +241,7 @@ class EmitGremlinTests(unittest.TestCase):
                 ContextField(base_location.navigate_to_field(u'name'), GraphQLString))),
             MarkLocation(child_location),
             Backtrack(base_location),
+            GlobalOperationsStart(),
             ConstructResult({
                 'foo_name': OutputContextField(base_name_location, GraphQLString),
             })
@@ -243,6 +275,7 @@ class EmitGremlinTests(unittest.TestCase):
             MarkLocation(child_location),
             Backtrack(base_location, optional=True),
             MarkLocation(revisited_base_location),
+            GlobalOperationsStart(),
             ConstructResult({
                 'bar_name': TernaryConditional(
                     BinaryComposition(
@@ -279,6 +312,7 @@ class EmitGremlinTests(unittest.TestCase):
         ir_blocks = [
             QueryRoot({'Event'}),
             MarkLocation(base_location),
+            GlobalOperationsStart(),
             ConstructResult({
                 'event_date': OutputContextField(base_event_date_location, GraphQLDateTime)
             })
