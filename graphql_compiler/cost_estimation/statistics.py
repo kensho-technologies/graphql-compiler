66147ee947b3a22ce45efdfa331c79eb18323834
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2019-07-26 13:51:28 -0400

    vertex_edge_vertex

--- graphql_compiler/cost_estimation/statistics.py
+++ graphql_compiler/cost_estimation/statistics.py
@@ -39,18 +39,49 @@ class Statistics(object):
         """
         raise NotImplementedError()
 
+    @abstractmethod
+    def get_vertex_edge_vertex_count(
+        self, vertex_source_class_name, edge_class_name, vertex_target_class_name
+    ):
+        """Return the count of edges of the given class connecting vertex_source to vertex_target.
+
+        This statistic is optional, as the estimator can roughly predict this statistic using
+        get_class_count(). In some cases of traversal between two vertices using an edge connecting
+        the vertices' superclasses, the estimates generated using get_class_count() may be off by
+        several orders of magnitude. In such cases, this statistic should be provided.
+
+        Args:
+            vertex_source_class_name: str, vertex class name.
+            edge_class_name: str, edge class name.
+            vertex_target_class_name: str, vertex class name.
+
+        Returns:
+            - int, count of edges of class edge_class with the two vertex classes as its endpoints
+                   if the statistic exists.
+            - None otherwise.
+        """
+        return None
+
 
 class LocalStatistics(Statistics):
     """Provides statistics using ones given at initialization."""
     def __init__(
-        self, class_counts
+        self, class_counts, vertex_edge_vertex_counts=None,
     ):
         """Initializes statistics with the given data.
 
         Args:
             class_counts: dict, str -> int, mapping vertex/edge class name to class count.
+            vertex_edge_vertex_counts: optional dict, (str, str, str) -> int, mapping
+                tuple of (vertex source class name, edge class name, vertex target class name) to
+                count of edge instances of given class connecting instances of two vertex classes.
         """
+        if vertex_edge_vertex_counts is None:
+            vertex_edge_vertex_counts = dict()
+
         self._class_counts = frozendict(class_counts)
+        self._vertex_edge_vertex_counts = frozendict(vertex_edge_vertex_counts)
+
 
     def get_class_count(self, class_name):
         """Return how many vertex or edge instances have, or inherit, the given class name.
@@ -69,3 +100,26 @@ class LocalStatistics(Statistics):
             raise AssertionError(u'Class count statistic is required, but entry not found for: '
                                  u'{}'.format(class_name))
         return self._class_counts[class_name]
+
+    def get_vertex_edge_vertex_count(
+        self, vertex_source_class_name, edge_class_name, vertex_target_class_name
+    ):
+        """Return the count of edges of the given class connecting vertex_source to vertex_target.
+
+        This statistic is optional, as the estimator can roughly predict this statistic using
+        get_class_count(). In some cases of traversal between two vertices using an edge connecting
+        the vertices' superclasses, the estimates generated using get_class_count() may be off by
+        several orders of magnitude. In such cases, this statistic should be provided.
+
+        Args:
+            vertex_source_class_name: str, vertex class name.
+            edge_class_name: str, edge class name.
+            vertex_target_class_name: str, vertex class name.
+
+        Returns:
+            - int, count of edges of class edge_class with the two vertex classes as its endpoints
+                   if the statistic exists.
+            - None otherwise.
+        """
+        statistic_key = (vertex_source_class_name, edge_class_name, vertex_target_class_name)
+        return self._vertex_edge_vertex_counts.get(statistic_key)
