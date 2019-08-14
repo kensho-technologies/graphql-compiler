9789b7186968eab21d5beb3a855c03321340d6ec
Qi Qi <qi.qi@kensho.com>
2019-08-14 13:37:15 -0400

    Remove unnecessary imports

--- graphql_compiler/schema_transformation/split_query.py
+++ graphql_compiler/schema_transformation/split_query.py
@@ -3,20 +3,14 @@ from collections import namedtuple
 from copy import copy
 
 from graphql.language.ast import (
-    Argument, Directive, Document, Field, InlineFragment, InterfaceTypeDefinition, Name,
-    ObjectTypeDefinition, OperationDefinition, SelectionSet, StringValue
+    Argument, Directive, Document, Field, InlineFragment, Name, OperationDefinition, SelectionSet,
+    StringValue
 )
-from graphql.language.visitor import TypeInfoVisitor, Visitor, visit
-from graphql.utils.type_info import TypeInfo
 
-from ..ast_manipulation import get_only_query_definition
 from ..compiler.helpers import strip_non_null_and_list_from_type
 from ..exceptions import GraphQLValidationError
 from ..schema import FilterDirective, OptionalDirective, OutputDirective
-from .utils import (
-    SchemaStructureError, check_query_is_valid_to_split, is_property_field_ast,
-    try_get_ast_by_name_and_type
-)
+from .utils import SchemaStructureError, is_property_field_ast, try_get_ast_by_name_and_type
 
 
 QueryConnection = namedtuple(
