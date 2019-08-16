35865cea575b4a407b5e7c687d5494f527056baa
Qi Qi <qi.qi@kensho.com>
2019-08-16 12:42:38 -0400

    Address comments, helper type name change

--- graphql_compiler/tests/schema_transformation_tests/test_split_query.py
+++ graphql_compiler/tests/schema_transformation_tests/test_split_query.py
@@ -6,41 +6,42 @@ from graphql import print_ast
 
 
 # The below namedtuple is used to check the structure of SubQueryNodes in tests
-ExampleQueryNode = namedtuple(
-    'ExampleQueryNode', (
+ExpectedQueryNode = namedtuple(
+    'ExpectedQueryNode', (
         'query_str',
         'schema_id',
         'child_query_nodes_and_out_names',
-        # List[Tuple[ExampleQueryNode, str, str]]
-        # child example query node, parent out name, child out name
+        # List[Tuple[ExpectedQueryNode, str, str]]
+        # child expected query node, parent out name, child out name
     )
 )
 
 
 class TestSplitQuery(unittest.TestCase):
-    def _check_query_node_structure(self, root_query_node, root_example_query_node):
-        """Check root_query_node has no parent and has the same structure as the example input."""
-        self.assertIs(root_query_node.parent_query_connection, None)
-        self._check_query_node_structure_helper(root_query_node, root_example_query_node)
+    def _check_query_node_structure(self, root_query_node, root_expected_query_node):
+        """Check root_query_node has no parent and has the same structure as the expected input."""
+        self.assertIsNone(root_query_node.parent_query_connection)
+        self._check_query_node_structure_helper(root_query_node, root_expected_query_node)
 
-    def _check_query_node_structure_helper(self, query_node, example_query_node):
-        """Check query_node has the same structure as example_query_node."""
+    def _check_query_node_structure_helper(self, query_node, expected_query_node):
+        """Check query_node has the same structure as expected_query_node."""
         # Check AST and id of the parent
-        self.assertEqual(print_ast(query_node.query_ast), example_query_node.query_str)
-        self.assertEqual(query_node.schema_id, example_query_node.schema_id)
+        self.assertEqual(print_ast(query_node.query_ast), expected_query_node.query_str)
+        self.assertEqual(query_node.schema_id, expected_query_node.schema_id)
         # Check number of children matches
-        self.assertEqual(len(query_node.child_query_connections),
-                         len(example_query_node.child_query_nodes_and_out_names))
-        for i in range(len(query_node.child_query_connections)):
+        child_query_connections = query_node.child_query_connections
+        expected_child_data = expected_query_node.child_query_nodes_and_out_names
+        self.assertEqual(len(child_query_connections), len(expected_child_data))
+        for i, (child_query_connection, expected_child_data_piece) in enumerate(
+            six.moves.zip(child_query_connections, expected_child_data)
+        ):
             # Check child and parent connections
-            child_query_connection = query_node.child_query_connections[i]
             child_query_node = child_query_connection.sink_query_node
-            child_example_query_node, parent_out_name, child_out_name = \
-                example_query_node.child_query_nodes_and_out_names[i]
+            child_expected_query_node, parent_out_name, child_out_name = expected_child_data_piece
             self._check_query_node_edge(query_node, i, child_query_node, parent_out_name,
                                         child_out_name)
             # Recurse
-            self._check_query_node_structure_helper(child_query_node, child_example_query_node)
+            self._check_query_node_structure_helper(child_query_node, child_expected_query_node)
 
     def _check_query_node_edge(self, parent_query_node, parent_to_child_edge_index,
                                child_query_node, parent_out_name, child_out_name):
