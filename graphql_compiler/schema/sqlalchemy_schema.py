599757e90169ea6eb8743d97cf6f2a67dbfc3cd1
Bojan Serafimov <bojan@kensho.com>
2019-08-16 14:55:30 -0400

    Add SQLAlchemyMetadata namedtuple

---
+++ graphql_compiler/schema/sqlalchemy_schema.py
@@ -0,0 +1,13 @@
+# Copyright 2019-present Kensho Technologies, LLC.
+from collections import namedtuple
+
+
+# Complete schema information sufficient to compile GraphQL queries to SQLAlchemy
+SQLAlchemyMetadata = namedtuple('SQLAlchemyMetadata', (
+    'schema',  # GraphQLSchema
+    'tables',  # dict mapping every graphql type in the schema to a sqlalchemy table
+    'joins',   # dict mapping every graphql type in the schema to:
+               #    dict mapping edge fields at that type to a dict with keys:
+               #        from_column_name: string, column name in the from_table to join on
+               #        to_column_name: string, column name in the to_table to join on
+))
