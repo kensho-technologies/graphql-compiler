6c91f090b12dfc0ab103c933c48098d3e1c0197f
Bojan Serafimov <bojan@kensho.com>
2019-08-16 15:56:48 -0400

    Address comments

--- graphql_compiler/schema/sqlalchemy_schema.py
+++ graphql_compiler/schema/sqlalchemy_schema.py
@@ -8,10 +8,10 @@ from collections import namedtuple
 # to perform joins for each vertex field. The property fields on each type are implicitly
 # mapped to columns with the same name on the corresponding table.
 SQLAlchemySchemaInfo = namedtuple('SQLAlchemySchemaInfo', (
-    'schema',  # GraphQLSchema
-    'tables',  # dict mapping every graphql type name in the schema to a sqlalchemy table
-    'joins',   # dict mapping every graphql type name in the schema to:
-               #    dict mapping every vertex field name at that type to a dict with keys:
-               #        from_column_name: string, column name to join from
-               #        to_column_name: string, column name to join to
+    'schema',     # GraphQLSchema
+    'tables',     # dict mapping every graphql object type name in the schema to a sqlalchemy table
+    'junctions',  # dict mapping every graphql object type name in the schema to:
+                  #    dict mapping every vertex field name at that type to a dict with keys:
+                  #        from_column_name: string, column name to join from
+                  #        to_column_name: string, column name to join to
 ))
