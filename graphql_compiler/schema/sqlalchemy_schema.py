016cf6ec3f1db0486b3c9529879fad01ceb196ca
Bojan Serafimov <bojan@kensho.com>
2019-08-16 15:23:57 -0400

    Add description

--- graphql_compiler/schema/sqlalchemy_schema.py
+++ graphql_compiler/schema/sqlalchemy_schema.py
@@ -3,6 +3,10 @@ from collections import namedtuple
 
 
 # Complete schema information sufficient to compile GraphQL queries to SQLAlchemy
+#
+# It describes the tables that correspond to each type, and gives instructions on how
+# to perform joins for each vertex field. The property fields on each type are implicitly
+# mapped to columns with the same name on the corresponding table.
 SQLAlchemySchemaInfo = namedtuple('SQLAlchemySchemaInfo', (
     'schema',  # GraphQLSchema
     'tables',  # dict mapping every graphql type name in the schema to a sqlalchemy table
