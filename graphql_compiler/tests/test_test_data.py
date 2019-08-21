f2c7a15ee68c4834c859076f6cda089833e57d6a
bojanserafimov <bojan.serafimov7@gmail.com>
2019-08-21 12:45:19 -0400

    Create SQLAlchemySchemaInfo used for testing (#497)
    * Create SQLAlchemySchemaInfo used for testing. WIP.
    
    * Include the whole schema in the sqlalchemy metadata
    
    * Add todos
    
    * Add SQL representation for set valued fields
    
    * Use compute_subclass_sets instead of hard-coding it
    
    * Lint
    
    * Remove empty line
    
    * Elaborate in docstring
    
    * Add dialect to SchemaInfo
    
    * Remove list-valued property fields from SQL schema
    
    * Remove unused line
    
    * Add DirectJoinDescriptor
    
    * Lint
    
    * Address some nits
    
    * Address comments
    
    * Add type_equivalence_hints to schema_info (#502)

---
+++ graphql_compiler/tests/test_test_data.py
@@ -0,0 +1,10 @@
+# Copyright 2019-present Kensho Technologies, LLC.
+import unittest
+
+from .test_helpers import get_sqlalchemy_schema_info
+
+
+class QueryFormattingTests(unittest.TestCase):
+    def test_sqlalchemy_schema_info(self):
+        # Test that validation passes
+        get_sqlalchemy_schema_info()
