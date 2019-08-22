15c4fd28bca4b3c6afbe75173863a41eec3bf7f4
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2019-08-21 22:45:51 -0400

    Things relating to ASTs are now in functions

--- graphql_compiler/query_pagination/parameter_generator.py
+++ graphql_compiler/query_pagination/parameter_generator.py
@@ -5,53 +5,75 @@ from uuid import UUID
 from graphql_compiler.compiler.helpers import get_parameter_name
 
 
-def _get_binary_filter_argument(filter_directive):
-    """Returns the argument name for a binary Filter Directive."""
+def _get_binary_filter_parameter(filter_directive):
+    """Return the parameter name for a binary Filter Directive."""
     filter_arguments = filter_directive.arguments[1].value.values
     if len(filter_arguments) != 1:
         raise AssertionError(u'Expected one argument in filter {}'.format(filter_directive))
-    return filter_arguments[0].value
+
+    argument_name = filter_arguments[0].value
+    parameter_name = get_parameter_name(argument_name)
+    return parameter_name
 
 
-def _get_ternary_filter_arguments(filter_directive):
-    """Returns the argument name for a ternary Filter Directive."""
+def _get_ternary_filter_parameters(filter_directive):
+    """Return the parameter names for a ternary Filter Directive."""
     filter_arguments = filter_directive.arguments[1].value.values
     if len(filter_arguments) != 2:
         raise AssertionError(u'Expected two arguments in filter {}'.format(filter_directive))
-    return [filter_arguments[0].value, filter_arguments[1].value]
+
+    argument_names = [filter_arguments[0].value, filter_arguments[1].value]
+    parameter_names = [
+        get_parameter_name(argument)
+        for argument in argument_names
+    ]
+    return parameter_names
+
+
+def _get_filter_operation(filter_directive):
+    """Return the @filter's op_name as a string."""
+    return filter_directive.arguments[0].value.value
 
 
 def _is_uuid_field(vertex_class, property_field):
-    """Stuff"""
+    """Return True if the given vertex's property field is a UUID."""
     return property_field == 'uuid'
 
 
-def _convert_uuid_string_to_int(uuid_string):
+def _decode_uuid_to_int(uuid_string):
     """Return the integer representation of a UUID string."""
     return UUID(uuid_string).int
+
+
+def _encode_int_as_uuid(uuid_int):
+    """Return the n-th lexicographical UUID, where n is the int argument."""
+    return str(UUID(int=int(uuid_int)))
 
 
 def _get_lower_and_upper_bound_of_related_filters(pagination_filter, user_parameters):
     """Stuff"""
     lower_bound, upper_bound = None, None
     for related_filter in pagination_filter.related_filters:
-        if related_filter.arguments[0].value.value == '<' or related_filter.arguments[0].value.value == '<=':
-            upper_bound = user_parameters[get_parameter_name(_get_binary_filter_argument(
-                related_filter
-            ))]
-        if related_filter.arguments[0].value.value == '>' or related_filter.arguments[0].value.value == '>=':
-            lower_bound = user_parameters[get_parameter_name(_get_binary_filter_argument(
-                related_filter
-            ))]
-        if related_filter.arguments[0].value.value == 'between':
-            argument_names = _get_ternary_filter_arguments(related_filter)
-            lower_bound = user_parameters[get_parameter_name(argument_names[0])]
-            upper_bound = user_parameters[get_parameter_name(argument_names[1])]
+        filter_operation = _get_filter_operation(related_filter)
+
+        if filter_operation == '<':
+            upper_bound = user_parameters[_get_binary_filter_parameter(related_filter)]
+        elif filter_operation == '<=':
+            upper_bound = user_parameters[_get_binary_filter_parameter(related_filter)]
+        elif filter_operation == '>':
+            lower_bound = user_parameters[_get_binary_filter_parameter(related_filter)]
+        elif filter_operation == '>=':
+            lower_bound = user_parameters[_get_binary_filter_parameter(related_filter)]
+        elif filter_operation == 'between':
+            parameter_names = _get_ternary_filter_parameters(related_filter)
+            lower_bound = user_parameters[parameter_names[0]]
+            upper_bound = user_parameters[parameter_names[1]]
 
     return lower_bound, upper_bound
 
 
 def _get_domain_of_field(vertex_class, field_name):
+    """Stuff"""
     if field_name == 'uuid':
         return '00000000-0000-0000-0000-000000000000', 'ffffffff-ffff-ffff-ffff-ffffffffffff'
 
@@ -93,21 +115,19 @@ def _generate_parameters_for_pagination_filters(
     if domain_lower_bound > domain_upper_bound:
         raise AssertionError(u'Invalid domain.')
 
-    lower_bound_int = _convert_uuid_string_to_int(domain_lower_bound)
-    upper_bound_int = _convert_uuid_string_to_int(domain_upper_bound)
+    lower_bound_int = _decode_uuid_to_int(domain_lower_bound)
+    upper_bound_int = _decode_uuid_to_int(domain_upper_bound)
 
     fraction_covered = float(1.0 / num_pages)
-
     proper_cut = lower_bound_int + (upper_bound_int - lower_bound_int) * fraction_covered
+    proper_cut_uuid_string = _encode_int_as_uuid(int(proper_cut))
 
-    proper_cut_uuid_string = str(UUID(int=int(proper_cut)))
-
-    next_page_query_parameter_name = get_parameter_name(_get_binary_filter_argument(
+    next_page_query_parameter_name = _get_binary_filter_parameter(
         pagination_filter.next_page_query_filter
-    ))
-    remainder_query_parameter_name = get_parameter_name(_get_binary_filter_argument(
+    )
+    remainder_query_parameter_name = _get_binary_filter_parameter(
         pagination_filter.remainder_query_filter
-    ))
+    )
 
     next_page_pagination_parameters[next_page_query_parameter_name] = proper_cut_uuid_string
     remainder_pagination_parameters[remainder_query_parameter_name] = proper_cut_uuid_string
@@ -118,8 +138,27 @@ def _generate_parameters_for_pagination_filters(
 def _validate_all_pagination_filters_have_parameters(
     pagination_filters, next_page_pagination_parameters, remainder_pagination_parameters
 ):
-    """Stuff"""
-    return None
+    """Validate that all PaginationFilters have assigned parameter values."""
+    for pagination_filter in pagination_filters:
+        next_page_arguments = _get_binary_filter_parameter(pagination_filter.next_page_query_filter)
+        remainder_arguments = _get_binary_filter_parameter(pagination_filter.next_page_query_filter)
+
+        next_page_filter_parameter_name = get_parameter_name(next_page_parameters[0])
+        remainder_filter_parameter_name = get_parameter_name(remainder_parameters[0])
+
+        if next_page_filter_parameter_name not in next_page_pagination_parameters:
+            raise AssertionError(u'Could not find parameter value for'
+                                 u'pagination parameter {} belonging to next page query: {} {} {}'
+                                 .format(next_page_filter_parameter_name, pagination_filters,
+                                         next_page_pagination_parameters,
+                                         remainder_pagination_parameters))
+
+        if remainder_filter_parameter_name not in next_page_pagination_parameters:
+            raise AssertionError(u'Could not find parameter value for'
+                                 u'pagination parameter {} belonging to next page query: {} {} {}'
+                                 .format(remainder_filter_parameter_name, pagination_filters,
+                                         next_page_pagination_parameters,
+                                         remainder_pagination_parameters))
 
 
 def generate_parameters_for_parameterized_query(
