63deb257bb7352df04459b21d21c466df85e3e08
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2019-08-21 21:59:45 -0400

    Improve code clarity

--- graphql_compiler/query_pagination/parameter_generator.py
+++ graphql_compiler/query_pagination/parameter_generator.py
@@ -5,6 +5,22 @@ from uuid import UUID
 from graphql_compiler.compiler.helpers import get_parameter_name
 
 
+def _get_binary_filter_argument(filter_directive):
+    """Returns the argument name for a binary Filter Directive."""
+    filter_arguments = filter_directive.arguments[1].value.values
+    if len(filter_arguments) != 1:
+        raise AssertionError(u'Expected one argument in filter {}'.format(filter_directive))
+    return filter_arguments[0].value
+
+
+def _get_ternary_filter_arguments(filter_directive):
+    """Returns the argument name for a ternary Filter Directive."""
+    filter_arguments = filter_directive.arguments[1].value.values
+    if len(filter_arguments) != 2:
+        raise AssertionError(u'Expected two arguments in filter {}'.format(filter_directive))
+    return [filter_arguments[0].value, filter_arguments[1].value]
+
+
 def _is_uuid_field(vertex_class, property_field):
     """Stuff"""
     return property_field == 'uuid'
@@ -19,13 +35,18 @@ def _get_lower_and_upper_bound_of_related_filters(pagination_filter, user_parame
     """Stuff"""
     lower_bound, upper_bound = None, None
     for related_filter in pagination_filter.related_filters:
-        if related_filter.op_name == '<' or related_filter.op_name == '<=':
-            upper_bound = user_parameters[get_parameter_name(related_filter['value'][0])]
-        if related_filter.op_name == '>' or related_filter.op_name == '>=':
-            lower_bound = user_parameters[get_parameter_name(related_filter['value'][0])]
-        if related_filter.op_name == 'between':
-            lower_bound = user_parameters[get_parameter_name(related_filter['value'][0])]
-            upper_bound = user_parameters[get_parameter_name(related_filter['value'][1])]
+        if related_filter.arguments[0].value.value == '<' or related_filter.arguments[0].value.value == '<=':
+            upper_bound = user_parameters[get_parameter_name(_get_binary_filter_argument(
+                related_filter
+            ))]
+        if related_filter.arguments[0].value.value == '>' or related_filter.arguments[0].value.value == '>=':
+            lower_bound = user_parameters[get_parameter_name(_get_binary_filter_argument(
+                related_filter
+            ))]
+        if related_filter.arguments[0].value.value == 'between':
+            argument_names = _get_ternary_filter_arguments(related_filter)
+            lower_bound = user_parameters[get_parameter_name(argument_names[0])]
+            upper_bound = user_parameters[get_parameter_name(argument_names[1])]
 
     return lower_bound, upper_bound
 
@@ -52,9 +73,11 @@ def _generate_parameters_for_pagination_filters(
     if not _is_uuid_field(pagination_filter.vertex_class, pagination_filter.property_field):
         raise AssertionError(u'Found pagination filter over vertex class {}'
                              u' and property field {}. Currently, only filters'
-                             u' over uuid property fields are allowed for pagination.'
+                             u' over uuid property fields are allowed for pagination: {}'
                              .format(pagination_filter.vertex_class,
-                                     pagination_filter.property_field))
+                                     pagination_filter.property_field,
+                                     pagination_filters))
+
 
     domain_lower_bound, domain_upper_bound = _get_domain_of_field(
         pagination_filter.vertex_class, pagination_filter.property_field
@@ -79,12 +102,13 @@ def _generate_parameters_for_pagination_filters(
 
     proper_cut_uuid_string = str(UUID(int=int(proper_cut)))
 
-    next_page_query_parameter_name = get_parameter_name(
-        pagination_filter.next_page_query_filter.arguments[1].value.values[0].value
-    )
-    remainder_query_parameter_name = get_parameter_name(
-        pagination_filter.remainder_query_filter.arguments[1].value.values[0].value
-    )
+    next_page_query_parameter_name = get_parameter_name(_get_binary_filter_argument(
+        pagination_filter.next_page_query_filter
+    ))
+    remainder_query_parameter_name = get_parameter_name(_get_binary_filter_argument(
+        pagination_filter.remainder_query_filter
+    ))
+
     next_page_pagination_parameters[next_page_query_parameter_name] = proper_cut_uuid_string
     remainder_pagination_parameters[remainder_query_parameter_name] = proper_cut_uuid_string
 
@@ -132,7 +156,9 @@ def generate_parameters_for_parameterized_query(
         pagination_filters, next_page_pagination_parameters, remainder_pagination_parameters
     )
 
-    # Since some of the user's parameters may have been parameterized
+    # Since the query parameterizer may have chosen some of the user's filters for pagination, it's
+    # possible that both a user parameter and a pagination parameter exist with the same parameter
+    # name. If such a conflict occurs, the pagination parameter is chosen as the parameter's value.
     next_page_parameters = deepcopy(user_parameters)
     next_page_parameters.update(next_page_pagination_parameters)
     remainder_parameters = deepcopy(user_parameters)
