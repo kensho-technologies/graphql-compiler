0cbb99842654be80fb26aed1d3c9b9c5446899bc
vlade <vlade.maksimovski@gmail.com>
2019-08-25 13:54:45 -0400

    Add one more test

--- graphql_compiler/tests/snapshot_tests/test_query_pagination.py
+++ graphql_compiler/tests/snapshot_tests/test_query_pagination.py
@@ -38,6 +38,7 @@ class QueryPaginationTests(unittest.TestCase):
             }
         }'''
         parameters = {}
+        page_size = 1
 
         count_data = {
             'Animal': 2,
@@ -82,7 +83,7 @@ class QueryPaginationTests(unittest.TestCase):
         expected_second_remainder = None
 
         received_first_next_page, received_first_remainder = (
-            paginate_query(schema_graph, statistics, test_data, parameters, 1)
+            paginate_query(schema_graph, statistics, test_data, parameters, page_size)
         )
 
         # Then we page the remainder we've received, to ensure paginating more than once is handled
@@ -91,7 +92,96 @@ class QueryPaginationTests(unittest.TestCase):
             paginate_query(
                 schema_graph, statistics,
                 received_first_remainder.query_string,
-                received_first_remainder.parameters, 1
+                received_first_remainder.parameters, page_size
+            )
+        )
+
+        self._compare_query_with_parameters_namedtuple(
+            expected_first_next_page, received_first_next_page
+        )
+        self._compare_query_with_parameters_namedtuple(
+            expected_first_remainder, received_first_remainder
+        )
+        self._compare_query_with_parameters_namedtuple(
+            expected_second_next_page, received_second_next_page
+        )
+        self._compare_query_with_parameters_namedtuple(
+            received_second_remainder, expected_second_remainder
+        )
+
+    @pytest.mark.usefixtures('snapshot_orientdb_client')
+    def test_pagination_with_filters_on_uuid(self):
+        """"Ensure pagination handles already-existing filters over uuid correctly."""
+        schema_graph = generate_schema_graph(self.orientdb_client)
+        test_data = '''{
+            Animal {
+            	uuid @filter(op_name: ">=", value: ["$uuid_filter"])
+                name @output(out_name: "animal")
+            }
+        }'''
+        parameters = {
+        	'uuid_filter': '80000000-0000-0000-0000-000000000000',
+        }
+        page_size = 4
+
+        count_data = {
+            'Animal': 8,
+        }
+
+        statistics = LocalStatistics(count_data)
+
+        expected_first_next_page = QueryStringWithParameters(
+            '''{
+                Animal {
+                    uuid @filter(op_name: ">=", value: ["$__paged_lower_bound_0"])
+                    	 @filter(op_name: "<", value: ["$__paged_upper_bound_0"])
+                    name @output(out_name: "animal")
+                }
+            }''',
+            {
+                '__paged_lower_bound_0': '80000000-0000-0000-0000-000000000000',
+                '__paged_upper_bound_0': 'c0000000-0000-0000-0000-000000000000',
+            },
+        )
+        expected_first_remainder = QueryStringWithParameters(
+            '''{
+                Animal {
+                    uuid @filter(op_name: ">=", value: ["$__paged_lower_bound_0"])
+                    name @output(out_name: "animal")
+                }
+            }''',
+            {
+                '__paged_lower_bound_0': 'c0000000-0000-0000-0000-000000000000',
+            },
+        )
+        expected_second_next_page = QueryStringWithParameters(
+            '''{
+                Animal {
+                    uuid @filter(op_name: ">=", value: ["$__paged_lower_bound_0"])
+                    name @output(out_name: "animal")
+                }
+            }''',
+            {
+                '__paged_lower_bound_0': 'c0000000-0000-0000-0000-000000000000',
+            },
+        )
+        # This query generates 2 pages, so the remainder after 2 pages is None.
+        expected_second_remainder = None
+
+        # Since the user has added '>=' filters on uuid, the paginator can just use it instead,
+        # making sure that the parameter name is changed to reflect that the filter is used for
+        # pagination.
+        received_first_next_page, received_first_remainder = (
+            paginate_query(schema_graph, statistics, test_data, parameters, page_size)
+        )
+
+        # Then we page the remainder we've received, to ensure paginating more than once is handled
+        # correctly.
+        received_second_next_page, received_second_remainder = (
+            paginate_query(
+                schema_graph, statistics,
+                received_first_remainder.query_string,
+                received_first_remainder.parameters, page_size
             )
         )
 
