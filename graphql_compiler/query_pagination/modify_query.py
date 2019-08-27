f83cdbb81870ec630a4b3946e76a48207fed32ca
Vladimir Maksimovski <ReaLNeroM@users.noreply.github.com>
2019-08-27 15:18:37 -0400

    Query pagination rewriter (#503)
    * Added terrible code
    
    * Fix crashes
    
    * Lotsa editing changes
    
    * Made the code actually work
    
    * Fix overwriting issues in the parameterizer
    
    * Made steps towards more immutable code
    
    * Made tests work for Visitor-ish idea
    
    * Work towards better copying of the query
    
    * Finished recursive idea, needs fleshing out
    
    * Added documentation for namedtuples
    
    * Finished with directives list copying
    
    * Delint
    
    * Commited local code
    
    * Delint
    
    * Address-ish comments
    
    * Made tests work
    
    * Finish separating node finding and filter modification generation
    
    * Add implementation of get_vertices_for_pagination
    
    * Delint
    
    * Remove erroneous validation check. Add tests for modify_query and filter_modification
    
    * Delint

--- graphql_compiler/query_pagination/modify_query.py
+++ graphql_compiler/query_pagination/modify_query.py
@@ -1,5 +1,13 @@
 # Copyright 2019-present Kensho Technologies, LLC.
 from collections import namedtuple
+from copy import copy
+
+from graphql.language.ast import (
+    Document, Field, InlineFragment, Name, OperationDefinition, SelectionSet
+)
+
+from graphql_compiler.ast_manipulation import get_only_query_definition
+from graphql_compiler.exceptions import GraphQLError
 
 
 # ParameterizedPaginationQueries namedtuple describes two query ASTs that have filters for
@@ -52,6 +60,150 @@ PaginationFilter = namedtuple(
 )
 
 
+def _get_field_with_name(ast, field_name):
+    """Search the AST selection set for a field with the given name, returning None if not found."""
+    if ast.selection_set is None:
+        return None
+
+    selections_list = ast.selection_set.selections
+    for selection in selections_list:
+        if selection.name.value == field_name:
+            return selection
+
+    return None
+
+
+def _create_field(field_name):
+    """Return a property field with the given name."""
+    property_field = Field(
+        alias=None, name=Name(value=field_name),
+        arguments=[], directives=[], selection_set=None,
+    )
+
+    return property_field
+
+
+def _add_next_page_query_filter_to_directives(directives_list, filter_modification):
+    """Return a directives list with the next page filter added. If already present, do nothing."""
+    if filter_modification.next_page_query_filter not in directives_list:
+        new_directives = copy(directives_list)
+        new_directives.append(filter_modification.next_page_query_filter)
+        return new_directives
+
+    return directives_list
+
+
+def _add_remainder_query_filter_to_directives(directives_list, filter_modification):
+    """Return a directives list with the remainder filter added."""
+    if filter_modification.remainder_query_filter not in directives_list:
+        new_directives = copy(directives_list)
+        new_directives.append(filter_modification.remainder_query_filter)
+        return new_directives
+
+    return directives_list
+
+
+def _add_pagination_filters_recursively(ast, parent_ast, filter_modifications, add_filter_func):
+    """Return an AST with pagination filters added.
+
+    Args:
+        ast: Document, AST of the current node.
+        parent_ast: Document, parent AST of the current node.
+        filter_modifications: List[FilterModification namedtuple], describing filters to be added.
+        add_filter_func: Function that applies filter modifications to list of directives. This
+                         function either adds the next page query's filter, or the remainder query's
+                         filter, depending on which query is currently being generated recursively.
+
+    Returns:
+        Document, AST with the given filter modifications performed.
+    """
+    if not isinstance(ast, (Field, InlineFragment, OperationDefinition)):
+        raise AssertionError(
+            u'Input AST is of type "{}", which should not be a selection.'
+            u''.format(type(ast).__name__)
+        )
+
+    if isinstance(ast, Field):
+        current_field_modifications = [
+            modification
+            for modification in filter_modifications
+            if modification.vertex is parent_ast and modification.property_field == ast.name.value
+        ]
+
+        if current_field_modifications != []:
+            # Add filter directives to property field
+            new_ast = copy(ast)
+
+            for modification in current_field_modifications:
+                new_directives = add_filter_func(ast.directives, modification)
+                new_ast.directives = new_directives
+
+            return new_ast
+
+    current_vertex_modifications = [
+        filter_modification
+        for filter_modification in filter_modifications
+        if filter_modification.vertex is ast
+    ]
+
+    made_changes = False
+    new_selections = []
+    # We first consider whether to add a property field, to make sure property fields precede vertex
+    # fields.
+    for modification in current_vertex_modifications:
+        # If the property field is not a Selection, add it to new_selections.
+        if not _get_field_with_name(ast, modification.property_field):
+            new_selection = _create_field(modification.property_field)
+            new_selection = _add_pagination_filters_recursively(
+                new_selection, ast, filter_modifications, add_filter_func
+            )
+
+            made_changes = True
+            new_selections.append(new_selection)
+
+    if ast.selection_set is not None:
+        for selection in ast.selection_set.selections:
+            new_selection = _add_pagination_filters_recursively(
+                selection, ast, filter_modifications, add_filter_func
+            )
+
+            if new_selection is not selection:  # Changes made somewhere down the line
+                made_changes = True
+
+            new_selections.append(new_selection)
+
+    if made_changes:
+        new_ast = copy(ast)
+        new_ast.selection_set = SelectionSet(selections=new_selections)
+        return new_ast
+    else:
+        return ast
+
+
+def _generate_pagination_filter(filter_modification):
+    """Create PaginationFilter namedtuple documenting filters usable for pagination."""
+    vertex_class = filter_modification.vertex.name.value
+    property_field_name = filter_modification.property_field
+    field = _get_field_with_name(filter_modification.vertex, property_field_name)
+
+    next_page_query_filter = filter_modification.next_page_query_filter
+    remainder_query_filter = filter_modification.remainder_query_filter
+    if field is None or field.directives is None:
+        already_existing_filters = []
+    else:
+        already_existing_filters = [
+            directive
+            for directive in field.directives
+            if directive.name.value == 'filter'
+        ]
+
+    pagination_filter = PaginationFilter(
+        vertex_class, property_field_name, next_page_query_filter, remainder_query_filter,
+        already_existing_filters
+    )
+    return pagination_filter
+
+
 def generate_parameterized_queries(
     schema_graph, statistics, query_ast, parameters, filter_modifications
 ):
@@ -73,4 +225,27 @@ def generate_parameterized_queries(
     Returns:
         ParameterizedPaginationQueries namedtuple.
     """
-    raise NotImplementedError()
+    query_type = get_only_query_definition(query_ast, GraphQLError)
+    next_page_type = _add_pagination_filters_recursively(
+        query_type, None, filter_modifications, _add_next_page_query_filter_to_directives
+    )
+    remainder_type = _add_pagination_filters_recursively(
+        query_type, None, filter_modifications, _add_remainder_query_filter_to_directives
+    )
+
+    next_page_ast = Document(
+        definitions=[next_page_type]
+    )
+    remainder_ast = Document(
+        definitions=[remainder_type]
+    )
+
+    pagination_filters = [
+        _generate_pagination_filter(modification)
+        for modification in filter_modifications
+    ]
+
+    parameterized_queries = ParameterizedPaginationQueries(
+        next_page_ast, remainder_ast, pagination_filters, parameters
+    )
+    return parameterized_queries
