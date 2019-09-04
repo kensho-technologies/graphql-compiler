62f5634009c6af53ae42fd523170e0d314842668
Pedro Mantica <pedro.mantica@kensho.com>
2019-09-04 16:36:37 -0400

    Add EdgeType objects to SchemaGraph

--- graphql_compiler/schema_generation/sqlalchemy/schema_graph_builder.py
+++ graphql_compiler/schema_generation/sqlalchemy/schema_graph_builder.py
@@ -1,5 +1,8 @@
 # Copyright 2019-present Kensho Technologies, LLC.
-from ..schema_graph import VertexType, PropertyDescriptor
+from ..schema_graph import (
+    VertexType, PropertyDescriptor, EdgeType, SchemaGraph, InheritanceStructure
+)
+from .scalar_type_mapper import try_get_graphql_scalar_type
 
 
 def get_sqlalchemy_schema_graph(vertex_name_to_table, direct_edges, junction_table_edges):
@@ -25,16 +28,33 @@ def get_sqlalchemy_schema_graph(vertex_name_to_table, direct_edges, junction_tab
     Returns:
         SchemaGraph reflecting the inputted metadata.
     """
-    pass
+    elements = {}
+    elements.update({
+        _get_vertex_type_from_sqlalchemy_table(vertex_name, table)
+        for vertex_name, table in vertex_name_to_table.items()
+    })
+    elements.update({
+        _get_edge_type_from_direct_edge(edge_name, direct_edge_descriptor)
+        for edge_name, direct_edge_descriptor in direct_edges.items()
+    })
+    direct_superclass_sets = {element_name: set() for element_name in elements}
+    SchemaGraph(elements, InheritanceStructure(direct_superclass_sets), set())
 
 
-def _get_vertex_type_from_sqlalchemy_table(table):
-    """Return the VertexType corresponding to the SQLALchemyTable object."""
+def _get_vertex_type_from_sqlalchemy_table(vertex_name, table):
+    """Return the VertexType corresponding to the SQLAlchemyTable object."""
     properties = dict()
-    for column in table.get_children():
+    for column in table.columns:
         name = column.key
         maybe_property_type = try_get_graphql_scalar_type(name, column.type)
         if maybe_property_type is not None:
             default = None
             properties[name] = PropertyDescriptor(maybe_property_type, default)
-    return VertexType(table.name, False, properties, {})
+    return VertexType(vertex_name, False, properties, {})
+
+
+def _get_edge_type_from_direct_edge(edge_name, direct_edge_descriptor):
+    """Return the EdgeType corresponding to a direct SQL edge."""
+    return EdgeType(edge_name, False, {}, {},
+                    base_in_connection=direct_edge_descriptor.from_vertex,
+                    base_out_connection=direct_edge_descriptor.to_vertex)
