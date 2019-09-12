5ea9144b54693b4e1673bbc356382b17fa1a760e
Pedro Mantica <pedro.mantica@kensho.com>
2019-09-12 15:38:15 -0400

    Nit

--- graphql_compiler/tests/test_helpers.py
+++ graphql_compiler/tests/test_helpers.py
@@ -673,6 +673,7 @@ def get_sqlalchemy_schema_info():
         for subclass in subclass_set:
             for edge_name, join_info in six.iteritems(join_descriptors.get(class_name, {})):
                 join_descriptors.setdefault(subclass, {})[edge_name] = join_info
+
     return make_sqlalchemy_schema_info(
         schema, type_equivalence_hints, mssql.dialect(), tables, join_descriptors)
 
