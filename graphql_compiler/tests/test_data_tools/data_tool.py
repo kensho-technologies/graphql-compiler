653a7bc44a6b823ed00cd56351fbab612239bded
Bojan Serafimov <bojan@kensho.com>
2019-09-24 14:00:29 -0400

    Implement backend agnostic integration data

--- graphql_compiler/tests/test_data_tools/data_tool.py
+++ graphql_compiler/tests/test_data_tools/data_tool.py
@@ -5,6 +5,7 @@ from glob import glob
 from os import path
 
 from funcy import retry
+from redisgraph import Edge, Node
 import six
 from sqlalchemy import create_engine, text
 from sqlalchemy.schema import CreateSchema
@@ -16,58 +17,144 @@ from ..integration_tests.integration_backend_config import (
 from ..test_helpers import get_sqlalchemy_schema_info
 
 
+def get_integration_data():
+    # Map vertex classes tuples containing the properties of each vertex of that class
+    vertex_values = {
+        'Animal': (
+            {
+                'uuid': 'cfc6e625-8594-0927-468f-f53d864a7a51',
+                'name': 'Animal 1',
+                'net_worth': 100,
+                'birthday': datetime.date(1900, 1, 1),
+            }, {
+                'uuid': 'cfc6e625-8594-0927-468f-f53d864a7a52',
+                'name': 'Animal 2',
+                'net_worth': 200,
+                'birthday': datetime.date(1950, 2, 2),
+            }, {
+                'uuid': 'cfc6e625-8594-0927-468f-f53d864a7a53',
+                'name': 'Animal 3',
+                'net_worth': 300,
+                'birthday': datetime.date(1975, 3, 3),
+            }, {
+                'uuid': 'cfc6e625-8594-0927-468f-f53d864a7a54',
+                'name': 'Animal 4',
+                'net_worth': 400,
+                'birthday': datetime.date(2000, 4, 4),
+            },
+        ),
+        'Species': (
+            {
+                'uuid': 'c2c14d8b-0e13-4e64-be63-c86704161850',
+                'name': 'Species 1',
+            }, {
+                'uuid': '35d33f6a-14ab-4b5c-a797-ee9ab817c1fb',
+                'name': 'Species 2',
+            },
+        ),
+    }
+
+    # Map edges to tuples containing the source and destination vertex of each edge of that class
+    edge_values = {
+        'Entity_Related': (
+            {
+                'from_uuid': 'c2c14d8b-0e13-4e64-be63-c86704161850',
+                'to_uuid': '35d33f6a-14ab-4b5c-a797-ee9ab817c1fb',
+            },
+        )
+    }
+
+    # Find the class name of each vertex uuid
+    uuid_to_class_name = {}
+    for vertex_name, values in six.iteritems(vertex_values):
+        for value in values:
+            if value['uuid'] in uuid_to_class_name:
+                raise AssertionError(u'Duplicate uuid found {}'.format(value['uuid']))
+            uuid_to_class_name[value['uuid']] = vertex_name
+
+    return vertex_values, edge_values, uuid_to_class_name
+
+
 def generate_orient_snapshot_data(client):
     """Create OrientDB test DB from the SQL commands file for snapshot testing."""
     project_root = path.dirname(path.abspath(__file__))
     sql_files = glob(path.join(project_root, 'snapshot_data/*.sql'))
+
     _load_sql_files_to_orient_client(client, sql_files)
 
 
 def generate_orient_integration_data(client):
     """Create OrientDB test DB from the SQL commands file for snapshot testing."""
-    project_root = path.dirname(path.abspath(__file__))
-    sql_files = glob(path.join(project_root, 'integration_data/*.sql'))
-    _load_sql_files_to_orient_client(client, sql_files)
+    vertex_values, edge_values, uuid_to_class_name = get_integration_data()
+    for vertex_name, vertices in six.iteritems(vertex_values):
+        for vertex_props in vertices:
+            # XXX SQL injection
+            # XXX string formatting is bad for some types
+            command = 'CREATE VERTEX {} SET '.format(vertex_name) + ', '.join(
+                '{} = \'{}\''.format(key, str(value))
+                for key, value in six.iteritems(vertex_props)
+            )
+            client.command(command)
+    for edge_name, edges in six.iteritems(edge_values):
+        for edge_spec in edges:
+            # XXX SQL injection
+            from_classname = uuid_to_class_name[edge_spec['from_uuid']]
+            to_classname = uuid_to_class_name[edge_spec['to_uuid']]
+            vertex_template = 'SELECT FROM {} WHERE uuid = \'{}\''
+            from_selection = vertex_template.format(from_classname, edge_spec['from_uuid'])
+            to_selection = vertex_template.format(to_classname, edge_spec['to_uuid'])
+            command = 'CREATE EDGE FROM ({}) TO ({})'.format(from_selection, to_selection)
+            client.command(command)
 
 
 def generate_neo4j_integration_data(client):
     """Create Neo4j test DB from the SQL commands file for integration testing."""
-    project_root = path.dirname(path.abspath(__file__))
-    neo4j_files = glob(path.join(project_root, 'integration_data/create_neo4j_integration.cypher'))
-    _load_neo4j_files_to_neo4j_client(client, neo4j_files)
-
-
-def _load_neo4j_files_to_neo4j_client(client, neo4j_files):
-    """Load list of supplied Cypher files into the supplied Neo4j client."""
-    for filepath in neo4j_files:
-        with open(filepath) as f:
-            with client.driver.session() as session:
-                for command in f.readlines():
-                    sanitized_command = command.strip()
-                    if len(sanitized_command) == 0 or sanitized_command[0] == '#':
-                        # comment or empty line, ignore
-                        continue
-                    session.run(sanitized_command)
+    vertex_values, edge_values, _ = get_integration_data()
+    with client.driver.session() as session:
+        session.run('match (n) detach delete n')  # XXX does this belong here?
+        for vertex_name, vertices in six.iteritems(vertex_values):
+            for vertex_props in vertices:
+                # XXX SQL injection. Validate keys are made of letters
+                command = 'create (:{} {{{}}})'.format(vertex_name, ', '.join(
+                    '{}: ${}'.format(key, key) for key in vertex_props))
+                session.run(command, vertex_props)
+        for edge_name, edges in six.iteritems(edge_values):
+            for edge_spec in edges:
+                # XXX SQL injection.
+                command = '''
+                    match (a {uuid: $from_uuid}) (b {uuid: $to_uuid})
+                    create (a)-[:$edge_name]->(b)'
+                '''
+                args = {
+                    'from_uuid': edge_spec['from_uuid'],
+                    'to_uuid': edge_spec['to_uuid'],
+                    'edge_name': edge_name,
+                }
+                session.run(command, args)
 
 
 def generate_redisgraph_integration_data(client):
     """Create Redisgraph test DB from the SQL commands file for integration testing."""
-    project_root = path.dirname(path.abspath(__file__))
-    cypher_files = glob(path.join(project_root,
-                                  'integration_data/create_redisgraph_integration.cypher'))
-    _load_cypher_files_to_redisgraph_client(client, cypher_files)
-
-
-def _load_cypher_files_to_redisgraph_client(client, cypher_files):
-    """Load list of supplied Cypher files into the supplied RedisGraph client."""
-    for filepath in cypher_files:
-        with open(filepath) as f:
-            for command in f.readlines():
-                sanitized_command = command.strip()
-                if len(sanitized_command) == 0 or sanitized_command[0] == '#':
-                    # comment or empty line, ignore
-                    continue
-                client.query(sanitized_command)
+    vertex_values, edge_values, _ = get_integration_data()
+    client.query('create (n)')  # XXX does this belong here?
+    client.query('match (n) delete n')  # XXX does this belong here?
+    uuid_to_node = {}
+    for vertex_name, vertices in six.iteritems(vertex_values):
+        for vertex_props in vertices:
+            uuid_to_node[vertex_props['uuid']] = Node(label=vertex_name, properties={
+                key: value
+                for key, value in six.iteritems(vertex_props)
+                if not isinstance(value, (datetime.date, datetime.datetime))
+            })
+            client.add_node(uuid_to_node[vertex_props['uuid']])
+    for edge_name, edges in six.iteritems(edge_values):
+        for edge_spec in edges:
+            client.add_edge(Edge(
+                uuid_to_node[edge_spec['from_uuid']],
+                edge_name,
+                uuid_to_node[edge_spec['to_uuid']],
+            ))
+    client.commit()
 
 
 def _load_sql_files_to_orient_client(client, sql_files):
@@ -144,60 +231,7 @@ def tear_down_integration_test_backends(sql_test_backends):
 def generate_sql_integration_data(sql_test_backends):
     """Populate test data for SQL backends for integration testing."""
     sql_schema_info = get_sqlalchemy_schema_info()
-
-    # Map vertex classes tuples containing the properties of each vertex of that class
-    vertex_values = {
-        'Animal': (
-            {
-                'uuid': 'cfc6e625-8594-0927-468f-f53d864a7a51',
-                'name': 'Animal 1',
-                'net_worth': Decimal('100'),
-                'birthday': datetime.date(1900, 1, 1),
-            }, {
-                'uuid': 'cfc6e625-8594-0927-468f-f53d864a7a52',
-                'name': 'Animal 2',
-                'net_worth': Decimal('200'),
-                'birthday': datetime.date(1950, 2, 2),
-            }, {
-                'uuid': 'cfc6e625-8594-0927-468f-f53d864a7a53',
-                'name': 'Animal 3',
-                'net_worth': Decimal('300'),
-                'birthday': datetime.date(1975, 3, 3),
-            }, {
-                'uuid': 'cfc6e625-8594-0927-468f-f53d864a7a54',
-                'name': 'Animal 4',
-                'net_worth': Decimal('400'),
-                'birthday': datetime.date(2000, 4, 4),
-            },
-        ),
-        'Species': (
-            {
-                'uuid': 'c2c14d8b-0e13-4e64-be63-c86704161850',
-                'name': 'Species 1',
-            }, {
-                'uuid': '35d33f6a-14ab-4b5c-a797-ee9ab817c1fb',
-                'name': 'Species 2',
-            },
-        ),
-    }
-
-    # Map edges to tuples containing the source and destination vertex of each edge of that class
-    edge_values = {
-        'Entity_Related': (
-            {
-                'from_uuid': 'c2c14d8b-0e13-4e64-be63-c86704161850',
-                'to_uuid': '35d33f6a-14ab-4b5c-a797-ee9ab817c1fb',
-            },
-        )
-    }
-
-    # Find the class name of each vertex uuid
-    uuid_to_class_name = {}
-    for vertex_name, values in six.iteritems(vertex_values):
-        for value in values:
-            if value['uuid'] in uuid_to_class_name:
-                raise AssertionError(u'Duplicate uuid found {}'.format(value['uuid']))
-            uuid_to_class_name[value['uuid']] = vertex_name
+    vertex_values, edge_values, uuid_to_class_name = get_integration_data()
 
     # Represent all edges as foreign keys
     uuid_to_foreign_key_values = {}
