c5b37561049b2745dab0f29499092da7095e9e63
Bojan Serafimov <bojan@kensho.com>
2019-10-04 12:40:40 -0400

    Add docstring

--- graphql_compiler/query_running.py
+++ graphql_compiler/query_running.py
@@ -1,4 +1,5 @@
 
 
 def run_sql_query(engine, compilation_result, parameters):
+    """Run a sqlalchemy query (with no retries )and format the result as a list of dicts."""
     return [dict(row) for row in engine.execute(compilation_result.query, parameters)]
