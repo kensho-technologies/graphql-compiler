3e3c606cbe40be9a2e2d6a836caa32c11194fc4c
Pedro Mantica <pedro.mantica@kensho.com>
2019-11-06 14:17:47 -0500

    Add Tristate class

--- graphql_compiler/global_utils.py
+++ graphql_compiler/global_utils.py
@@ -15,3 +15,46 @@ def merge_non_overlapping_dicts(merge_target, new_data):
         result[key] = value
 
     return result
+
+
+class Tristate:
+    """A class designed to implement three-value logic."""
+
+    def __init__(self, value):
+        """Return a Tristate instance.
+
+        Args:
+            value: Optional[bool, None]
+
+        Returns:
+            A Tristate instance.
+        """
+        if value not in [True, False, None]:
+            raise AssertionError("Tristate value must be either True, False, or None.")
+        self._value = value
+
+    @property
+    def value(self):
+        """Return True, False or None depending on whether the value is True, False or Unknown."""
+        return self._value
+
+    def __bool__(self):
+        raise ValueError("Cannot implicitly convert Tristate to a boolean.")
+
+    def __nonzero__(self):  # Python2 bool function.
+        return self.__bool__()
+
+    def __eq__(self, other):
+        if not isinstance(other, Tristate):
+            return False
+        else:
+            return self.value == other.value
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return 'Tristate({})'.format(self.value)
