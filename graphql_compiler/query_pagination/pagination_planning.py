d0eb765c59f292881940a7c9b30cbfc72c128fea
Bojan Serafimov <bojan@kensho.com>
2019-11-07 15:53:05 -0500

    WIP

--- graphql_compiler/query_pagination/pagination_planning.py
+++ graphql_compiler/query_pagination/pagination_planning.py
@@ -8,7 +8,7 @@ from ..exceptions import GraphQLError
 # The intent to split the query at a certain vertex into a certain number of pages.
 VertexPartition = namedtuple(
     'VertexPartition', (
-        'query_path',  # List[field name : str] leading to the vertex to be split
+        'query_path',  # Tuple[field name : str] leading to the vertex to be split
         'number_of_splits',  # The number of subdivisions intended for this vertex
     )
 )
@@ -33,8 +33,19 @@ def get_pagination_plan(schema_info, query_ast, number_of_pages):
     #                       - The root node has a unique index
     #                       - There are only a few different vertices at the root
     pagination_node = get_only_selection_from_ast(definition_ast, GraphQLError)
-    query_path = [pagination_node.name.value]
 
-    if pagination_node.name.value not in schema_info.pagination_keys:
+    pagination_field = schema_info.pagination_keys.get(pagination_node.name.value)
+    if pagination_field is None:
         return None
-    return PaginationPlan([VertexPartition(query_path, number_of_pages)])
+
+    if is_uuid_field:
+        pass
+    elif is_int_field:
+        quantiles = schema_info.pagination_keys.statistics.get_field_quantiles(
+            pagination_node, pagination_field)
+        if quantiles is None or len(quantiles) < 10 * number_of_pages:
+            return None
+    else:
+        return None
+
+    return PaginationPlan([VertexPartition([pagination_node], number_of_pages)])
