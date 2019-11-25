e565c11f6b607234fcd6e445daaad6d00962773c
Bojan Serafimov <bojan@kensho.com>
2019-11-25 10:54:34 -0500

    Remove unused code

--- graphql_compiler/query_pagination/query_parameterizer.py
+++ graphql_compiler/query_pagination/query_parameterizer.py
@@ -11,46 +11,6 @@ from graphql.language.ast import (
 )
 
 
-RESERVED_PARAMETER_PREFIX = '_paged_'
-
-# ParameterizedPaginationQueries namedtuple, describing two query ASTs that have PaginationFilters
-# describing filters with which the query result size can be controlled. Note that these filters are
-# returned parameterized i.e. values for the filters' parameters have yet to be generated.
-# Additionally, a dict containing user-defined parameters is stored. Since this function may modify
-# the user parameters to ensure better pagination, the user_parameters dict may differ from the
-# original query's parameters that were provided to the paginator.
-ParameterizedPaginationQueries = namedtuple(
-    'ParameterizedPaginationQueries',
-    (
-        'next_page_query',          # Document, AST of query that will return the next page of
-                                    # results when combined with pagination parameters.
-        'remainder_query',          # Document, AST of query that will return the remainder of
-                                    # results when combined with pagination parameters.
-        'pagination_filters',       # List[PaginationFilter], filters usable for pagination.
-        'user_parameters',          # dict, parameters that the user has defined for other filters.
-    ),
-)
-
-# PaginationFilter namedtuples document filters usable for pagination purposes within the larger
-# context of a ParameterizedPaginationQueries namedtuple. These filters may either be added by the
-# query parameterizer, or filters that the user has added whose parameter values may be modified for
-# generating paginated queries.
-PaginationFilter = namedtuple(
-    'PaginationFilter',
-    (
-        'vertex_class',                 # str, vertex class to which the property field belongs to.
-        'property_field',               # str, name of the property field filtering is done over.
-        'next_page_query_filter',       # Directive, filter directive with '<' operator usable
-                                        # for pagination in the page query.
-        'remainder_query_filter',       # Directive, filter directive with '>=' operator usable
-                                        # for pagination in the remainder query.
-        'related_filters',              # List[Directive], filter directives that share the same
-                                        # vertex and property field as the next_page_query_filter,
-                                        # and are used to generate more accurate pages.
-    ),
-)
-
-
 def _add_pagination_filters(query_ast, query_path, pagination_field, lower_page):
     if not isinstance(query_ast, (Field, InlineFragment, OperationDefinition)):
         raise AssertionError(
