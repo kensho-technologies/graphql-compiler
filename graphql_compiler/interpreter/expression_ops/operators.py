c366c9fb7f1fd5b412d2d00b0fa0e424aad605bd
Predrag Gruevski <predrag@kensho.com>
2019-12-26 15:41:45 -0500

    Add null handling in operators.

--- graphql_compiler/interpreter/expression_ops/operators.py
+++ graphql_compiler/interpreter/expression_ops/operators.py
@@ -15,12 +15,12 @@ def apply_operator(operator: str, left_value: Any, right_value: Any) -> Any:
     elif operator == '>=':
         return left_value >= right_value
     elif operator == 'contains' or operator == 'has_substring':
-        return right_value in left_value
+        return left_value is not None and right_value in left_value
     elif operator == 'not_contains':
-        return right_value not in left_value
+        return left_value is not None and right_value not in left_value
     elif operator == 'starts_with':
-        return left_value.startswith(right_value)
+        return left_value is not None and left_value.startswith(right_value)
     elif operator == 'ends_with':
-        return left_value.endswith(right_value)
+        return left_value is not None and left_value.endswith(right_value)
     else:
         raise NotImplementedError()
