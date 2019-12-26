68c6ee07cb653f329d4b2233f8e24e98208464f8
Predrag Gruevski <predrag@kensho.com>
2019-12-26 15:37:06 -0500

    Handle "has_substring" and null check operators.

--- graphql_compiler/interpreter/expression_ops/operators.py
+++ graphql_compiler/interpreter/expression_ops/operators.py
@@ -3,12 +3,18 @@ from typing import Any
 
 def apply_operator(operator: str, left_value: Any, right_value: Any) -> Any:
     if operator == '=':
-        return left_value == right_value
+        if right_value is None:
+            return left_value is None
+        else:
+            return left_value == right_value
     elif operator == '!=':
-        return left_value != right_value
+        if right_value is None:
+            return left_value is not None
+        else:
+            return left_value != right_value
     elif operator == '>=':
         return left_value >= right_value
-    elif operator == 'contains':
+    elif operator == 'contains' or operator == 'has_substring':
         return right_value in left_value
     elif operator == 'not_contains':
         return right_value not in left_value
