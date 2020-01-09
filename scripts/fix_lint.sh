0c3e9794a5c0394ea795496e02fc1aab4c930a65
Bojan Serafimov <bojan@kensho.com>
2020-01-09 16:24:42 -0500

    Check if inside pipenv shell

--- scripts/fix_lint.sh
+++ scripts/fix_lint.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 # Copyright 2018-present Kensho Technologies, LLC.
 
+# Assert script is running inside pipenv shell
+if [[ "$VIRTUAL_ENV" == "" ]]
+then
+    echo "Please run pipenv shell first"
+    exit 1
+fi
+
 # Make sure the current working directory for this script is the root directory.
 cd "$(git -C "$(dirname "${0}")" rev-parse --show-toplevel )"
 
