1db37e1896ce37696231b257e16e0c9130b07953
bojanserafimov <bojan.serafimov7@gmail.com>
2020-01-09 16:31:21 -0500

    Add common bash settings
    Co-Authored-By: Predrag Gruevski <predrag@kensho.com>

--- scripts/fix_lint.sh
+++ scripts/fix_lint.sh
@@ -8,6 +8,13 @@ then
     exit 1
 fi
 
+# Exit non-zero on errors, undefined variables, and errors in pipelines.
+set -euo pipefail
+
+# Ensure that the "**" glob operator is applied recursively.
+# Make globs that do not match return null values.
+shopt -s globstar nullglob
+
 # Make sure the current working directory for this script is the root directory.
 cd "$(git -C "$(dirname "${0}")" rev-parse --show-toplevel )"
 
