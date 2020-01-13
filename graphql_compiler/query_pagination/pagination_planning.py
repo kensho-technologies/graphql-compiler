319e9597e458ac4cad4d3cdb568daea64c589121
Bojan Serafimov <bojan@kensho.com>
2020-01-13 16:37:48 -0500

    Separate function

--- graphql_compiler/query_pagination/pagination_planning.py
+++ graphql_compiler/query_pagination/pagination_planning.py
@@ -1,7 +1,7 @@
 # Copyright 2019-present Kensho Technologies, LLC.
 from abc import ABC
 from dataclasses import dataclass, field
-from typing import Tuple
+from typing import Tuple, Optional
 
 from graphql import DocumentNode
 
@@ -76,6 +76,72 @@ def get_plan_page_count(plan: PaginationPlan) -> int:
     return number_of_pages
 
 
+def get_pagination_capacity(
+    schema_info: QueryPlanningSchemaInfo, pagination_node: str, number_of_pages: int
+):
+    """Get the pagination capacity of a node, and suggest how it can be improved to meet demand."""
+    pagination_field = schema_info.pagination_keys.get(pagination_node)
+    if pagination_field is None:
+        return None, (PaginationFieldNotSpecified(pagination_node),)
+
+    # Ideally, we paginate into the desired number of pages, and don't advise for any changes.
+    # These two values are mutated in the code below if this vertex is not ideal in any way.
+    page_capacity = number_of_pages
+    advisories: Tuple[PaginationAdvisory, ...] = tuple()
+
+    if is_uuid4_type(schema_info, pagination_node, pagination_field):
+        # UUIDs are uniformly distributed, but it is not wise to make more pages than
+        # there are values.
+        class_count = schema_info.statistics.get_class_count(pagination_node)
+        page_capacity = min(page_capacity, class_count)
+    elif field_supports_range_reasoning(schema_info, pagination_node, pagination_field):
+        # If the ratio len(quantiles) // number_of_pages is N, then the largest page is
+        # expected to be (N + 1) / N times larger than the smallest page. This is because
+        # if we assume that quantiles divide the domain into equal chunks, some pages will
+        # span over N + 1 chunks, and some will span over N chunks. So if N is 5,
+        # (N + 1) / N = 1.2, which is not too bad.
+        ideal_min_num_quantiles_per_page = 5
+        ideal_quantile_resolution = ideal_min_num_quantiles_per_page * number_of_pages + 1
+
+        quantiles = schema_info.statistics.get_field_quantiles(pagination_node, pagination_field)
+        if quantiles is None:
+            # If there's no quantiles, we don't paginate. We could try to assume an uniform
+            # value distribution and make some pagination plan with more than 1 page, but
+            # the cost estimator would need to match the behavior and assume uniform value
+            # distribution when estimating the selectivity of range filters.
+            page_capacity = 1
+        elif len(quantiles) - 1 < number_of_pages:
+            # If we have some quantiles but not enough, we generate a page for each chunk
+            # of values separated by the quantiles.
+            page_capacity = len(quantiles) - 1
+
+        # If we have less than the ideal number of quantiles, we request for more.
+        if quantiles is None or len(quantiles) < ideal_quantile_resolution:
+            current_quantile_resolution = 0 if quantiles is None else len(quantiles)
+            advisories = (
+                InsufficientQuantiles(
+                    pagination_node,
+                    pagination_field,
+                    current_quantile_resolution,
+                    ideal_quantile_resolution,
+                ),
+            )
+    else:
+        vertex_type = schema_info.schema.get_type(pagination_node.name.value)
+        field_type_name = vertex_type.fields[pagination_field].type.name
+        raise AssertionError(
+            u"Cannot paginate on {}.{} because pagination on {} is not supported ".format(
+                pagination_node.name.value, pagination_field, field_type_name
+            )
+        )
+
+    # Construct and return the plan and advisories.
+    plan = (VertexPartitionPlan((pagination_node,), pagination_field, page_capacity),)
+    if page_capacity <= 1:
+        plan = None
+    return plan, advisories
+
+
 def get_pagination_plan(
     schema_info: QueryPlanningSchemaInfo, query_ast: DocumentNode, number_of_pages: int
 ) -> Tuple[PaginationPlan, Tuple[PaginationAdvisory, ...]]:
@@ -103,68 +169,12 @@ def get_pagination_plan(
     #                       - The root node has no pagination_key
     #                       - The root node has a unique index
     #                       - There are only a few different vertices at the root
-    pagination_node = get_only_selection_from_ast(definition_ast, GraphQLError).name.value
+    root_node = get_only_selection_from_ast(definition_ast, GraphQLError).name.value
+    pagination_node = root_node
+    vertex_partition_plan, advisories = get_pagination_capacity(
+        schema_info, pagination_node, number_of_pages)
 
-    pagination_field = schema_info.pagination_keys.get(pagination_node)
-    if pagination_field is None:
-        return PaginationPlan(tuple()), (PaginationFieldNotSpecified(pagination_node),)
-
-    if is_uuid4_type(schema_info, pagination_node, pagination_field):
-        return (
-            PaginationPlan(
-                (VertexPartitionPlan((pagination_node,), pagination_field, number_of_pages),)
-            ),
-            tuple(),
-        )
-    elif field_supports_range_reasoning(schema_info, pagination_node, pagination_field):
-        quantiles = schema_info.statistics.get_field_quantiles(pagination_node, pagination_field)
-
-        # If the ratio len(quantiles) // number_of_pages is N, then the largest page is
-        # expected to be (N + 1) / N times larger than the smallest page. This is because
-        # if we assume that quantiles divide the domain into equal chunks, some pages will
-        # span over N + 1 chunks, and some will span over N chunks. So if N is 5,
-        # (N + 1) / N = 1.2, which is not too bad.
-        ideal_min_num_quantiles_per_page = 5
-        ideal_quantile_resolution = ideal_min_num_quantiles_per_page * number_of_pages + 1
-
-        # Ideally, we paginate into the desired number of pages.
-        page_capacity = number_of_pages
-        if quantiles is None:
-            # If there's no quantiles, we don't paginate. We could try to assume an uniform
-            # value distribution and make some pagination plan with more than 1 page, but
-            # the cost estimator would need to match the behavior and assume uniform value
-            # distribution when estimating the selectivity of range filters.
-            page_capacity = 1
-        elif len(quantiles) - 1 < number_of_pages:
-            # If we have some quantiles but not enough, we generate a page for each chunk
-            # of values separated by the quantiles.
-            page_capacity = len(quantiles) - 1
-
-        # If we have less than the ideal number of quantiles, we request for more.
-        advisories: Tuple[PaginationAdvisory, ...] = tuple()
-        if quantiles is None or len(quantiles) < ideal_quantile_resolution:
-            current_quantile_resolution = 0 if quantiles is None else len(quantiles)
-            advisories = (
-                InsufficientQuantiles(
-                    pagination_node,
-                    pagination_field,
-                    current_quantile_resolution,
-                    ideal_quantile_resolution,
-                ),
-            )
-
-        # Construct and return the plan and advisories.
-        plan = PaginationPlan(
-            (VertexPartitionPlan((pagination_node,), pagination_field, page_capacity),)
-        )
-        if page_capacity == 1:
-            plan = PaginationPlan(tuple())
-        return plan, advisories
+    if vertex_partition_plan is None:
+        return PaginationPlan(tuple(),), advisories
     else:
-        vertex_type = schema_info.schema.get_type(pagination_node.name.value)
-        field_type_name = vertex_type.fields[pagination_field].type.name
-        raise AssertionError(
-            u"Cannot paginate on {}.{} because pagination on {} is not supported ".format(
-                pagination_node.name.value, pagination_field, field_type_name
-            )
-        )
+        return PaginationPlan(vertex_partition_plan,), advisories
