345fb11b563cabdc012797f6d075144eb19b3c07
Bojan Serafimov <bojan@kensho.com>
2020-01-14 10:41:18 -0500

    Add test for name generation

---
+++ graphql_compiler/tests/test_global_utils.py
@@ -0,0 +1,17 @@
+# Copyright 2020-present Kensho Technologies, LLC.
+import unittest
+
+from ..global_utils import find_new_name
+
+class QueryPaginationTests(unittest.TestCase):
+    """Test the global_utils module"""
+
+    def test_find_new_name(self):
+        taken_names = ["animal_0", "animal_1"]
+
+        new_name = find_new_name("animal", taken_names, try_original=False)
+        self.assertTrue(new_name not in taken_names)
+
+        new_name = find_new_name("animal", taken_names, try_original=True)
+        self.assertTrue(new_name not in taken_names)
+        self.assertEqual("animal", new_name)
