fc76bb414ee653da94bb235e74770c73422587fb
Bojan Serafimov <bojan@kensho.com>
2020-01-24 14:31:46 -0500

    Add pagination analysis

---
+++ graphql_compiler/cost_estimation/analysis.py
@@ -0,0 +1,90 @@
+from .filter_selectivity_utils import get_integer_interval_for_filters_on_field
+from ..cost_estimation.interval import Interval, measure_int_interval, intersect_int_intervals
+from ..cost_estimation.int_value_conversion import convert_field_value_to_int, convert_int_to_field_value, MIN_UUID_INT, MAX_UUID_INT, field_supports_range_reasoning, is_uuid4_type
+from ..schema import is_meta_field
+
+
+def _convert_int_interval_to_field_value_interval(schema_info, vertex_type, field, interval):
+    lower_bound = None
+    upper_bound = None
+    if interval.lower_bound is not None:
+        lower_bound = convert_int_to_field_value(
+            schema_info, vertex_type, field, interval.lower_bound)
+    if interval.upper_bound is not None:
+        upper_bound = convert_int_to_field_value(
+            schema_info, vertex_type, field, interval.upper_bound)
+    return Interval(lower_bound, upper_bound)
+
+
+def get_field_value_intervals(schema_info, query_metadata, parameters):
+    """Map each (query_path, field) tuple to its field value interval or None if not supported."""
+    field_value_intervals = {}
+    for location, location_info in query_metadata.registered_locations:
+        filter_infos = query_metadata.get_filter_infos(location)
+        vertex_type_name = location_info.type.name
+
+        # Group filters by field
+        single_field_filters = {}
+        for filter_info in filter_infos:
+            if len(filter_info.fields) == 0:
+                raise AssertionError(u"Got filter on 0 fields {} {}".format(filter_info, location_name))
+            elif len(filter_info.fields) == 1:
+                single_field_filters.setdefault(filter_info.fields[0], []).append(filter_info)
+            else:
+                pass  # We don't do anything for multi-field filters yet
+
+        # Find field_value_interval for each field
+        for field_name, filters_on_field in single_field_filters.items():
+            filters_on_field = [
+                filter_info
+                for filter_info in filter_infos
+                if filter_info.fields == (field_name,)
+            ]
+
+            integer_interval = get_integer_interval_for_filters_on_field(
+                schema_info, filters_on_field, vertex_type_name, field_name, parameters)
+            field_value_interval = _convert_int_interval_to_field_value_interval(
+                schema_info, vertex_type_name, field_name, integer_interval)
+
+            field_value_intervals[(location.query_path, field_name)] = field_value_interval
+    return field_value_intervals
+
+
+def get_pagination_capacities(schema_info, field_value_intervals, query_metadata, parameters):
+    """Get the pagination capacity for each eligible pagination field."""
+    pagination_capacities = {}
+    for location, location_info in query_metadata.registered_locations:
+        vertex_type_name = location_info.type.name
+
+        for field_name, field in location_info.type.fields.items():
+            key = (location.query_path, field_name)
+            if not is_meta_field(field_name):
+                if is_uuid4_type(schema_info, vertex_type_name, field_name):
+                    # TODO multiply by filter selectivity
+                    pagination_capacities[key] = schema_info.statistics.get_class_count(vertex_type_name)
+                elif field_supports_range_reasoning(schema_info, vertex_type_name, field_name):
+                    # TODO write test for this case
+                    field_value_interval = field_value_intervals.get(key, Interval(None, None))
+                    quantiles = schema_info.statistics.get_field_quantiles(vertex_type_name, field_name)
+                    if quantiles is not None:
+
+                        # Since we can't be sure the minimum observed value is the
+                        # actual minimum value, we treat values less than it as part
+                        # of the first quantile. That's why we drop the minimum and
+                        # maximum observed values from the quantile list.
+                        proper_quantiles = quantiles[1:-1]
+
+                        # Get the relevant quantiles (ones inside the field_value_interval)
+                        min_quantile = 0
+                        max_quantile = len(proper_quantiles)
+                        if field_value_interval.lower_bound is not None:
+                            min_quantile = bisect.bisect_left(proper_quantiles, field_value_interval.lower_bound)
+                        if field_value_interval.upper_bound is not None:
+                            max_quantile = bisect.bisect_left(proper_quantiles, field_value_interval.upper_bound)
+                        relevant_quantiles = proper_quantiles[min_quantile:max_quantile]
+
+                        # TODO take into account duplicate quantile values
+
+                        pagination_capacities[key] = len(relevant_quantiles) + 1
+
+    return pagination_capacities
---
+++ graphql_compiler/tests/snapshot_tests/test_cost_estimation_analysis.py
@@ -0,0 +1,112 @@
+# Copyright 2019-present Kensho Technologies, LLC.
+import datetime
+from typing import Any, Dict, Tuple
+import unittest
+
+import pytest
+
+from ...cost_estimation.interval import Interval
+from ...compiler.compiler_frontend import graphql_to_ir
+from ...ast_manipulation import safe_parse_graphql
+from ...cost_estimation import analysis
+from ...cost_estimation.statistics import LocalStatistics
+from ...query_pagination import QueryStringWithParameters, paginate_query
+from ...query_pagination.pagination_planning import (
+    InsufficientQuantiles,
+    PaginationAdvisory,
+    PaginationPlan,
+    VertexPartitionPlan,
+    get_pagination_plan,
+)
+from ...schema.schema_info import QueryPlanningSchemaInfo
+from ...schema_generation.graphql_schema import get_graphql_schema_from_schema_graph
+from ..test_helpers import generate_schema_graph
+
+
+# The following TestCase class uses the 'snapshot_orientdb_client' fixture
+# which pylint does not recognize as a class member.
+# pylint: disable=no-member
+@pytest.mark.slow
+class CostEstimationAnalysisTests(unittest.TestCase):
+    """Test the cost estimation module using standard input data when possible."""
+
+    @pytest.mark.usefixtures("snapshot_orientdb_client")
+    def test_get_field_value_intervals(self) -> None:
+        schema_graph = generate_schema_graph(self.orientdb_client)  # type: ignore  # from fixture
+        graphql_schema, type_equivalence_hints = get_graphql_schema_from_schema_graph(schema_graph)
+        pagination_keys = {vertex_name: "uuid" for vertex_name in schema_graph.vertex_class_names}
+        uuid4_fields = {vertex_name: {"uuid"} for vertex_name in schema_graph.vertex_class_names}
+        class_counts = {"Animal": 1000}
+        statistics = LocalStatistics(class_counts)
+        schema_info = QueryPlanningSchemaInfo(
+            schema=graphql_schema,
+            type_equivalence_hints=type_equivalence_hints,
+            schema_graph=schema_graph,
+            statistics=statistics,
+            pagination_keys=pagination_keys,
+            uuid4_fields=uuid4_fields,
+        )
+
+        # Check that the correct plan is generated when it's obvious (page the root)
+        graphql_query_string = """{
+            Animal {
+                name @output(out_name: "animal_name")
+                uuid @filter(op_name: ">", value: ["$uuid_min"])
+            }
+        }"""
+        parameters = {
+            "uuid_min": "80000000-0000-0000-0000-000000000000",
+        }
+        number_of_pages = 10
+        query_metadata = graphql_to_ir(
+            schema_info.schema,
+            graphql_query_string,
+            type_equivalence_hints=schema_info.type_equivalence_hints
+        ).query_metadata_table
+
+        ints = analysis.get_field_value_intervals(schema_info, query_metadata, parameters)
+        expected_ints = {
+            (('Animal',), 'uuid'): Interval(
+                lower_bound='80000000-0000-0000-0000-000000000001',
+                upper_bound=None)
+        }
+        self.assertEqual(expected_ints, ints)
+
+    @pytest.mark.usefixtures("snapshot_orientdb_client")
+    def test_get_pagination_capacities(self) -> None:
+        schema_graph = generate_schema_graph(self.orientdb_client)  # type: ignore  # from fixture
+        graphql_schema, type_equivalence_hints = get_graphql_schema_from_schema_graph(schema_graph)
+        pagination_keys = {vertex_name: "uuid" for vertex_name in schema_graph.vertex_class_names}
+        uuid4_fields = {vertex_name: {"uuid"} for vertex_name in schema_graph.vertex_class_names}
+        class_counts = {"Animal": 1000}
+        statistics = LocalStatistics(class_counts)
+        schema_info = QueryPlanningSchemaInfo(
+            schema=graphql_schema,
+            type_equivalence_hints=type_equivalence_hints,
+            schema_graph=schema_graph,
+            statistics=statistics,
+            pagination_keys=pagination_keys,
+            uuid4_fields=uuid4_fields,
+        )
+
+        # Check that the correct plan is generated when it's obvious (page the root)
+        graphql_query_string = """{
+            Animal {
+                name @output(out_name: "animal_name")
+                uuid @filter(op_name: ">", value: ["$uuid_min"])
+            }
+        }"""
+        parameters = {
+            "uuid_min": "80000000-0000-0000-0000-000000000000",
+        }
+        number_of_pages = 10
+        query_metadata = graphql_to_ir(
+            schema_info.schema,
+            graphql_query_string,
+            type_equivalence_hints=schema_info.type_equivalence_hints
+        ).query_metadata_table
+
+        ints = analysis.get_field_value_intervals(schema_info, query_metadata, parameters)
+        caps = analysis.get_pagination_capacities(schema_info, ints, query_metadata, parameters)
+        expected_caps = {(('Animal',), 'uuid'): 1000}
+        self.assertEqual(expected_caps, caps)
