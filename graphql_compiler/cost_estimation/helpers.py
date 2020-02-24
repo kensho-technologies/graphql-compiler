f0d8c550868e09eb130e66c189908f91977b9aba
bojanserafimov <bojan.serafimov7@gmail.com>
2020-02-24 12:06:24 -0500

    Use assertion instead of cast (#760)
    * Use assertion instead of cast
    
    * Lint
    
    * Lint

--- graphql_compiler/cost_estimation/helpers.py
+++ graphql_compiler/cost_estimation/helpers.py
@@ -1,5 +1,5 @@
 # Copyright 2019-present Kensho Technologies, LLC.
-from typing import Union, cast
+from typing import Union
 
 from graphql import (
     GraphQLInt,
@@ -14,38 +14,39 @@ from ..schema import GraphQLDate, GraphQLDateTime
 from ..schema.schema_info import QueryPlanningSchemaInfo
 
 
-def _get_field_type(
+def _get_property_field_type(
     schema_info: QueryPlanningSchemaInfo, vertex_name: str, field_name: str
 ) -> Union[GraphQLList, GraphQLScalarType]:
-    """Get the GraphQL type of the field on the specified vertex."""
-    return (
-        cast(
-            Union[GraphQLObjectType, GraphQLInterfaceType], schema_info.schema.get_type(vertex_name)
+    """Get the GraphQL type of the property field on the specified vertex."""
+    vertex_type = schema_info.schema.get_type(vertex_name)
+    if not isinstance(vertex_type, (GraphQLObjectType, GraphQLInterfaceType)):
+        raise AssertionError(
+            f"Found unexpected type for vertex {vertex_name}: {vertex_type} {type(vertex_type)}"
         )
-        .fields[field_name]
-        .type
-    )
+    return vertex_type.fields[field_name].type
 
 
 def is_datetime_field_type(
     schema_info: QueryPlanningSchemaInfo, vertex_name: str, field_name: str
 ) -> bool:
     """Return whether the field is of type GraphQLDateTime."""
-    return is_same_type(GraphQLDateTime, _get_field_type(schema_info, vertex_name, field_name))
+    return is_same_type(
+        GraphQLDateTime, _get_property_field_type(schema_info, vertex_name, field_name)
+    )
 
 
 def is_date_field_type(
     schema_info: QueryPlanningSchemaInfo, vertex_name: str, field_name: str
 ) -> bool:
     """Return whether the field is of type GraphQLDate."""
-    return is_same_type(GraphQLDate, _get_field_type(schema_info, vertex_name, field_name))
+    return is_same_type(GraphQLDate, _get_property_field_type(schema_info, vertex_name, field_name))
 
 
 def is_int_field_type(
     schema_info: QueryPlanningSchemaInfo, vertex_name: str, field_name: str
 ) -> bool:
     """Return whether the field is of type GraphQLInt."""
-    return is_same_type(GraphQLInt, _get_field_type(schema_info, vertex_name, field_name))
+    return is_same_type(GraphQLInt, _get_property_field_type(schema_info, vertex_name, field_name))
 
 
 def is_uuid4_type(schema_info: QueryPlanningSchemaInfo, vertex_name: str, field_name: str) -> bool:
