ac56f7e33279045421478013f920fa27aecef48e
bojanserafimov <bojan.serafimov7@gmail.com>
2020-02-25 15:17:34 -0500

    Fix bug with impossible pagination (#762)
    * Write failing test case
    
    * Update comment
    
    * Fix bug
    
    * Lint
    
    * Update docstring

--- graphql_compiler/query_pagination/query_splitter.py
+++ graphql_compiler/query_pagination/query_splitter.py
@@ -3,25 +3,19 @@ from typing import Tuple
 
 from ..cost_estimation.analysis import QueryPlanningAnalysis
 from ..global_utils import ASTWithParameters
-from .pagination_planning import PaginationAdvisory, get_pagination_plan
+from .pagination_planning import PaginationPlan
 from .parameter_generator import generate_parameters_for_vertex_partition
 from .query_parameterizer import generate_parameterized_queries
 
 
 def split_into_page_query_and_remainder_query(
-    query_analysis: QueryPlanningAnalysis, num_pages: int
-) -> Tuple[ASTWithParameters, ASTWithParameters, Tuple[PaginationAdvisory, ...]]:
+    query_analysis: QueryPlanningAnalysis, pagination_plan: PaginationPlan
+) -> Tuple[ASTWithParameters, ASTWithParameters]:
     """Split a query into two equivalent queries, one of which will return roughly a page of data.
-
-    First, two parameterized queries are generated that contain filters usable for pagination i.e.
-    filters with which the number of results can be constrained. Parameters for these filters are
-    then generated such that one of the new queries will return roughly a page of results, while the
-    other query will generate the rest of the results. This ensures that the two new queries' result
-    data is equivalent to the original query's result data.
 
     Args:
         query_analysis: the query with any query analysis needed for pagination
-        num_pages: int, number of pages to split the query into.
+        pagination_plan: plan on how to split the query. The plan defines what is considered a page
 
     Returns:
         tuple containing three elements:
@@ -30,18 +24,7 @@ def split_into_page_query_and_remainder_query(
             - ASTWithParameters or None, describing a query that returns the rest of the
               result data of the original query. If the original query is expected to return only a
               page or less of results, then this element will have value None.
-            - Tuple of PaginationAdvisories that communicate what can be done to improve pagination
     """
-    if num_pages <= 1:
-        raise AssertionError(
-            u"Could not split query {} into pagination queries for the next page"
-            u" of results, as the number of pages {} must be greater than 1".format(
-                query_analysis.ast_with_parameters, num_pages
-            )
-        )
-
-    # TODO propagate advisories to top-level
-    pagination_plan, advisories = get_pagination_plan(query_analysis, num_pages)
     if len(pagination_plan.vertex_partitions) != 1:
         raise NotImplementedError(
             u"We only support pagination plans with one vertex partition. "
@@ -61,4 +44,4 @@ def split_into_page_query_and_remainder_query(
         pagination_plan.vertex_partitions[0],
         first_param,
     )
-    return page_query, remainder_query, advisories
+    return page_query, remainder_query
