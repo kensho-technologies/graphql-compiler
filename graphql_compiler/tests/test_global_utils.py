07ab6298058fac3b18485f961901bd5f9aa6d8c4
Pedro Mantica <pedro.mantica@kensho.com>
2020-04-30 13:47:02 -0400

    Revert "Remove global utils testing"
    This reverts commit 3b389024b5e8863fbbf01f0a1abe083e222fe451.

---
+++ graphql_compiler/tests/test_global_utils.py
@@ -0,0 +1,25 @@
+import unittest
+
+from ..global_utils import assert_set_equality
+
+
+class GlobalUtilTests(unittest.TestCase):
+    def test_assert_equality(self):
+        # Matching sets
+        assert_set_equality({"a", "b"}, {"a", "b"})
+
+        # Additional keys in the first set
+        with self.assertRaises(AssertionError):
+            assert_set_equality({"a", "b"}, {"b"})
+
+        # Additional keys in the second type
+        with self.assertRaises(AssertionError):
+            assert_set_equality({"b"}, {"a", "b"})
+
+        # Different sets with same number of elements
+        with self.assertRaises(AssertionError):
+            assert_set_equality({"a", "b"}, {"c", "b"})
+
+        # Different types
+        with self.assertRaises(AssertionError):
+            assert_set_equality({"a"}, {1})
