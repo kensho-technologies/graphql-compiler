e934877e25ca8b856eedeaed96552512d6c42005
Predrag Gruevski <predrag@kensho.com>
2020-05-26 16:02:35 -0400

    Implement all the filter operators.

--- graphql_compiler/interpreter/expression_ops/operators.py
+++ graphql_compiler/interpreter/expression_ops/operators.py
@@ -1,26 +1,61 @@
-from typing import Any
+from typing import Any, Callable, Optional
+
+
+# Define the various operators' behavior for values other than None.
+# The behavior with respect to None is defined explicitly in the "apply_operator()" function.
+_operator_definitions_for_non_null_values = {
+    "=": lambda left, right: left == right,
+    ">": lambda left, right: left > right,
+    ">=": lambda left, right: left >= right,
+    "<": lambda left, right: left < right,
+    "<=": lambda left, right: left <= right,
+    "!=": lambda left, right: left != right,
+    "contains": lambda left, right: right in left,
+    "not_contains": lambda left, right: right not in left,
+    "has_substring": lambda left, right: right in left,
+    "starts_with": lambda left, right: left.startswith(right),
+    "ends_with": lambda left, right: left.endswith(right),
+    "in_collection": lambda left, right: left in right,
+}
 
 
 def apply_operator(operator: str, left_value: Any, right_value: Any) -> Any:
-    if operator == '=':
-        if right_value is None:
-            return left_value is None
+    # SQL-like semantics: comparisons with "None" generally produce False unless comparing to None:
+    # - None is equal to None
+    # - None != <anything other than None> is True
+    # - None is not greater than, nor less than, any other value
+    # - None contains nothing and is never contained in anything
+    # - None starts/ends with nothing, and nothing starts or ends with None
+    #
+    # TODO(predrag): Implement the "filtering with non-existing tag from optional scope passes" rule
+    #                See matching TODO note in the handling of Filter IR blocks.
+    left_none = left_value is None
+    right_none = right_value is None
+
+    if left_none and right_none:
+        if operator in {"=", ">=", "<="}:
+            # The operation simplifies to None = None, which we define as True.
+            return True
+        elif operator == "!=":
+            # We have None != None, which is False.
+            return False
         else:
-            return left_value == right_value
-    elif operator == '!=':
-        if right_value is None:
-            return left_value is not None
+            # All other comparisons vs None produce False.
+            return False
+    elif left_none or right_none:
+        # Only one of the values is None. We define this to always be False, for all operators.
+        return False
+    elif not left_none and not right_none:
+        # Neither side of the operator is None, apply operator normally.
+        operator_handler: Optional[Callable[[Any, Any], Any]] = (
+            _operator_definitions_for_non_null_values.get(operator, None)
+        )
+        if operator_handler is not None:
+            return operator_handler(left_value, right_value)
         else:
-            return left_value != right_value
-    elif operator == '>=':
-        return left_value >= right_value
-    elif operator == 'contains' or operator == 'has_substring':
-        return left_value is not None and right_value in left_value
-    elif operator == 'not_contains':
-        return left_value is not None and right_value not in left_value
-    elif operator == 'starts_with':
-        return left_value is not None and left_value.startswith(right_value)
-    elif operator == 'ends_with':
-        return left_value is not None and left_value.endswith(right_value)
-    else:
-        raise NotImplementedError()
+            raise NotImplementedError(f"Operator {operator} is not currently implemented.")
+
+    raise AssertionError(
+        f"Unreachable code reached: (left operator right) "
+        f"was ({left_value} {operator} {right_value})"
+    )
