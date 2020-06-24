3e4ac37ddabd09f82a0912e4786b09a77dd8a878
Leon Wu <lwgithub@gmail.com>
2020-06-24 10:37:31 -0400

    Add enum suppression test

--- graphql_compiler/tests/schema_transformation_tests/input_schema_strings.py
+++ graphql_compiler/tests/schema_transformation_tests/input_schema_strings.py
@@ -68,6 +68,32 @@ class InputSchemaStrings(object):
     """
     )
 
+    extra_enum_schema = dedent(
+        """\
+        schema {
+          query: SchemaQuery
+        }
+
+        type Droid {
+          height: Height
+        }
+
+        type SchemaQuery {
+          Droid: Droid
+        }
+
+        enum Height {
+          TALL
+          SHORT
+        }
+
+        enum Size {
+          BIG
+          SMALL
+        }
+    """
+    )
+
     interface_schema = dedent(
         """\
         schema {
