90fa3d752a7ef85d1cc39b7388b25152900e3386
Predrag Gruevski <predrag@kensho.com>
2020-07-27 14:55:41 -0400

    Add EdgeInfo to the interpreter __init__.py file.

--- graphql_compiler/interpreter/__init__.py
+++ graphql_compiler/interpreter/__init__.py
@@ -1,2 +1,2 @@
 from .api import interpret_ir  # noqa
-from .typedefs import DataContext, DataToken, InterpreterAdapter  # noqa
+from .typedefs import DataContext, DataToken, EdgeInfo, InterpreterAdapter  # noqa
