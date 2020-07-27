1a2814fd167ba1851d14ee44f0d444ff246c40b4
Predrag Gruevski <predrag@kensho.com>
2020-07-27 14:43:23 -0400

    Suppress false-positive unused import errors.

--- graphql_compiler/typedefs.py
+++ graphql_compiler/typedefs.py
@@ -4,10 +4,15 @@ from typing import Union
 from graphql import GraphQLList, GraphQLNonNull, GraphQLScalarType
 
 
+# The below code is an import shim for TypedDict: we don't want to conditionally import it from
+# every file that needs it. Instead, we conditionally import it here and then import from this file
+# in every other location where this is needed.
+#
+# Hence, the "unused import" warnings on TypedDict here are false-positives.
 try:
-    from typing import TypedDict
+    from typing import TypedDict  # noqa  # pylint: disable=unused-import
 except ImportError:  # TypedDict was only added to typing in Python 3.8
-    from typing_extensions import TypedDict
+    from typing_extensions import TypedDict  # noqa  # pylint: disable=unused-import
 
 
 # The compiler's supported GraphQL types for query arguments. The GraphQL type of a query argument
