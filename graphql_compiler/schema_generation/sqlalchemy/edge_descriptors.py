e72787763ae20149efd853e240fe35ceaadfa55e
Pedro Mantica <pedro.mantica@kensho.com>
2020-08-13 11:18:14 -0400

    Nit

--- graphql_compiler/schema_generation/sqlalchemy/edge_descriptors.py
+++ graphql_compiler/schema_generation/sqlalchemy/edge_descriptors.py
@@ -74,7 +74,7 @@ def generate_direct_edge_descriptors_from_foreign_keys(
     Return:
         set of edge descriptors. An edge descriptor is generated for each single-column foreign key
         and primary key pair. For instance, suppose there is a foreign key A.b_id referencing
-        primary key B.b_id, and that V_a and V_b are the vertex type names of table A and B
+        primary key B.b_id, and that V_a and V_b are the vertex type names of table A and B,
         respectively. Then this function would generate the following edge descriptor:
         DirectEdgeDescriptor(
             from_vertex=V_a,
