ca96b617c4b95a50a05a30b1dd2ad065e79721cf
Predrag Gruevski <predrag@kensho.com>
2020-08-14 16:31:46 -0400

    Annotate simple test methods to return None. (#907)
    * Annotate simple test methods to return None.
    
    * Tighten mypy.ini.

--- graphql_compiler/tests/schema_generation_tests/test_sqlalchemy_schema_generation.py
+++ graphql_compiler/tests/schema_generation_tests/test_sqlalchemy_schema_generation.py
@@ -97,35 +97,35 @@ class SQLAlchemySchemaInfoGenerationTests(unittest.TestCase):
             graphql_schema, type_equivalence_hints, dialect, vertex_name_to_table, join_descriptors
         )
 
-    def test_table_vertex_representation(self):
+    def test_table_vertex_representation(self) -> None:
         self.assertIsInstance(self.schema_info.schema.get_type("Table1"), GraphQLObjectType)
 
-    def test_table_vertex_representation_with_non_default_name(self):
+    def test_table_vertex_representation_with_non_default_name(self) -> None:
         self.assertIsInstance(
             self.schema_info.schema.get_type("ArbitraryObjectName"), GraphQLObjectType
         )
 
-    def test_represent_supported_fields(self):
+    def test_represent_supported_fields(self) -> None:
         table1_graphql_object = self.schema_info.schema.get_type("Table1")
         self.assertEqual(
             table1_graphql_object.fields["column_with_supported_type"].type, GraphQLString
         )
 
-    def test_ignored_fields_not_supported(self):
+    def test_ignored_fields_not_supported(self) -> None:
         table1_graphql_object = self.schema_info.schema.get_type("Table1")
         self.assertTrue("column_with_non_supported_type" not in table1_graphql_object.fields)
 
-    def test_warn_when_type_is_not_supported(self):
+    def test_warn_when_type_is_not_supported(self) -> None:
         with pytest.warns(Warning):
             try_get_graphql_scalar_type("binary", LargeBinary())
 
-    def test_support_sql_tz_naive_datetime_types(self):
+    def test_support_sql_tz_naive_datetime_types(self) -> None:
         column_name = "tz_naive_datetime"
         tz_naive_types = (DateTime(timezone=False), TIMESTAMP(timezone=False))
         for sql_type in tz_naive_types:
             self.assertEqual(GraphQLDateTime, try_get_graphql_scalar_type(column_name, sql_type))
 
-    def test_do_not_support_sql_tz_aware_datetime_types(self):
+    def test_do_not_support_sql_tz_aware_datetime_types(self) -> None:
         column_name = "tz_aware_datetime"
         tz_aware_types = (DateTime(timezone=True), TIMESTAMP(timezone=True))
         for sql_type in tz_aware_types:
@@ -133,11 +133,11 @@ class SQLAlchemySchemaInfoGenerationTests(unittest.TestCase):
                 graphql_type = try_get_graphql_scalar_type(column_name, sql_type)
             self.assertIsNone(graphql_type)
 
-    def test_mssql_scalar_type_representation(self):
+    def test_mssql_scalar_type_representation(self) -> None:
         table1_graphql_object = self.schema_info.schema.get_type("Table1")
         self.assertEqual(table1_graphql_object.fields["column_with_mssql_type"].type, GraphQLInt)
 
-    def test_direct_sql_edge_representation(self):
+    def test_direct_sql_edge_representation(self) -> None:
         table1_graphql_object = self.schema_info.schema.get_type("Table1")
         arbitrarily_named_graphql_object = self.schema_info.schema.get_type("ArbitraryObjectName")
         self.assertEqual(
@@ -147,7 +147,7 @@ class SQLAlchemySchemaInfoGenerationTests(unittest.TestCase):
             arbitrarily_named_graphql_object.fields["in_test_edge"].type.of_type.name, "Table1"
         )
 
-    def test_get_join_descriptors(self):
+    def test_get_join_descriptors(self) -> None:
         expected_join_descriptors = {
             "Table1": {
                 "out_test_edge": DirectJoinDescriptor("source_column", "destination_column")
@@ -158,7 +158,7 @@ class SQLAlchemySchemaInfoGenerationTests(unittest.TestCase):
         }
         self.assertEqual(expected_join_descriptors, self.schema_info.join_descriptors)
 
-    def test_basic_index_generation_from_primary_key(self):
+    def test_basic_index_generation_from_primary_key(self) -> None:
         indexes = self.schema_graph.get_all_indexes_for_class("Table1")
         self.assertIn(
             IndexDefinition(
@@ -172,7 +172,7 @@ class SQLAlchemySchemaInfoGenerationTests(unittest.TestCase):
             indexes,
         )
 
-    def test_index_generation_from_multi_column_primary_key(self):
+    def test_index_generation_from_multi_column_primary_key(self) -> None:
         indexes = self.schema_graph.get_all_indexes_for_class("TableWithMultiplePrimaryKeyColumns")
         self.assertEqual(
             {
@@ -188,11 +188,11 @@ class SQLAlchemySchemaInfoGenerationTests(unittest.TestCase):
             indexes,
         )
 
-    def test_index_generation_from_primary_key_with_an_unsupported_column_type(self):
+    def test_index_generation_from_primary_key_with_an_unsupported_column_type(self) -> None:
         indexes = self.schema_graph.get_all_indexes_for_class("TableWithNonSupportedPrimaryKeyType")
         self.assertEqual(frozenset(), indexes)
 
-    def test_index_generation_from_unique_constraint(self):
+    def test_index_generation_from_unique_constraint(self) -> None:
         indexes = self.schema_graph.get_all_indexes_for_class("Table1")
         self.assertIn(
             IndexDefinition(
@@ -209,7 +209,7 @@ class SQLAlchemySchemaInfoGenerationTests(unittest.TestCase):
 
 @pytest.mark.filterwarnings("ignore: Ignored .* edges implied by composite foreign keys.*")
 class SQLAlchemyForeignKeyEdgeGenerationTests(unittest.TestCase):
-    def test_edge_generation_from_foreign_keys(self):
+    def test_edge_generation_from_foreign_keys(self) -> None:
         metadata = MetaData()
 
         table1 = Table(
@@ -242,7 +242,7 @@ class SQLAlchemyForeignKeyEdgeGenerationTests(unittest.TestCase):
             },
         )
 
-    def test_warning_for_ignored_foreign_keys(self):
+    def test_warning_for_ignored_foreign_keys(self) -> None:
         metadata = MetaData()
 
         table1 = Table(
@@ -281,7 +281,7 @@ class SQLAlchemySchemaInfoGenerationErrorTests(unittest.TestCase):
     def setUp(self):
         self.vertex_name_to_table = _get_test_vertex_name_to_table()
 
-    def test_reference_to_non_existent_source_vertex(self):
+    def test_reference_to_non_existent_source_vertex(self) -> None:
         direct_edges = {
             "invalid_source_vertex": DirectEdgeDescriptor(
                 "InvalidVertexName", "source_column", "ArbitraryObjectName", "destination_column"
@@ -290,7 +290,7 @@ class SQLAlchemySchemaInfoGenerationErrorTests(unittest.TestCase):
         with self.assertRaises(InvalidSQLEdgeError):
             get_sqlalchemy_schema_info(self.vertex_name_to_table, direct_edges, dialect())
 
-    def test_reference_to_non_existent_destination_vertex(self):
+    def test_reference_to_non_existent_destination_vertex(self) -> None:
         direct_edges = {
             "invalid_source_vertex": DirectEdgeDescriptor(
                 "Table1", "source_column", "InvalidVertexName", "destination_column"
@@ -299,7 +299,7 @@ class SQLAlchemySchemaInfoGenerationErrorTests(unittest.TestCase):
         with self.assertRaises(InvalidSQLEdgeError):
             get_sqlalchemy_schema_info(self.vertex_name_to_table, direct_edges, dialect())
 
-    def test_reference_to_non_existent_source_column(self):
+    def test_reference_to_non_existent_source_column(self) -> None:
         direct_edges = {
             "invalid_source_vertex": DirectEdgeDescriptor(
                 "Table1", "invalid_column_name", "ArbitraryObjectName", "destination_column"
@@ -308,7 +308,7 @@ class SQLAlchemySchemaInfoGenerationErrorTests(unittest.TestCase):
         with self.assertRaises(InvalidSQLEdgeError):
             get_sqlalchemy_schema_info(self.vertex_name_to_table, direct_edges, dialect())
 
-    def test_reference_to_non_existent_destination_column(self):
+    def test_reference_to_non_existent_destination_column(self) -> None:
         direct_edges = {
             "invalid_destination_column": DirectEdgeDescriptor(
                 "Table1", "source_column", "ArbitraryObjectName", "invalid_column_name"
@@ -317,7 +317,7 @@ class SQLAlchemySchemaInfoGenerationErrorTests(unittest.TestCase):
         with self.assertRaises(InvalidSQLEdgeError):
             get_sqlalchemy_schema_info(self.vertex_name_to_table, direct_edges, dialect())
 
-    def test_missing_primary_key(self):
+    def test_missing_primary_key(self) -> None:
         table_without_primary_key = Table(
             "TableWithoutPrimaryKey", MetaData(), Column("arbitrary_column", String()),
         )
@@ -325,7 +325,7 @@ class SQLAlchemySchemaInfoGenerationErrorTests(unittest.TestCase):
         with self.assertRaises(MissingPrimaryKeyError):
             get_sqlalchemy_schema_info(faulty_vertex_name_to_table, {}, dialect())
 
-    def test_missing_multiple_primary_keys(self):
+    def test_missing_multiple_primary_keys(self) -> None:
         metadata: MetaData = MetaData()
         table_without_primary_key: Table = Table(
             "TableWithoutPrimaryKey", metadata, Column("arbitrary_column", String()),
