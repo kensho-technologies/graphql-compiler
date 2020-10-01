4ea00c3fd5aebbc50a93d6208d70fe9ebfa79e9a
Predrag Gruevski <predrag@kensho.com>
2020-10-01 12:02:03 -0400

    Fix line endings.

--- graphql_compiler/interpreter/typedefs.py
+++ graphql_compiler/interpreter/typedefs.py
@@ -81,7 +81,7 @@ class InterpreterAdapter(Generic[DataToken], metaclass=ABCMeta):
     - project_neighbors() is similar: for an iterable of DataContexts and a specific edge name,
       it returns an iterable (data_context, iterable_of_neighbor_tokens) where
       iterable_of_neighbor_tokens yields a DataToken for each vertex that can be reached by
-      following the specified edge from data_context's vertex. 
+      following the specified edge from data_context's vertex.
     - can_coerce_to_type() is used to check whether a DataToken corresponding to one vertex type
       can be safely converted into one representing a different vertex type. Given an iterable of
       DataContexts and the name of the type to which the conversion is attempted, it produces
@@ -93,7 +93,7 @@ class InterpreterAdapter(Generic[DataToken], metaclass=ABCMeta):
     optimizations to either happen automatically or be available with minimal additional work.
     A few simple examples:
     - Interpreters perform lazy evaluation by default: if exactly 3 query results are requested,
-      then only the minimal data necessary to produce *exactly 3 results' worth* of outputs is loaded.
+      then only the minimal data necessary for *exactly 3* results' worth of outputs is loaded.
     - When computing a particular result, data loading for output fields is deferred
       until *after* all filtering operations have been completed, to minimize data loads.
     - Data caching is easy to implement within this API -- simply have
