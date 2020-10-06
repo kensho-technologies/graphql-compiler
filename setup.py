662e7756b2f7df8e540175248081fdaa89a4ba63
Predrag Gruevski <predrag@kensho.com>
2020-10-06 13:50:35 -0400

    Add Python 3.9 to our testing matrix. (#943)
    * Add Python 3.9 to our testing matrix.
    
    * Add Python 3.9 to our compatibility list.

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="graphql database compiler sql orientdb",
     python_requires=">=3.6",
