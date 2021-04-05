79f99f49ec74c9bbfcd80924b2a2f3e7149f45f0
LWprogramming <LWprogramming@users.noreply.github.com>
2021-04-05 12:13:59 -0400

    Update graphql_compiler/schema_transformation/rename_schema.py
    Co-authored-by: chewselene <52711428+chewselene@users.noreply.github.com>

--- graphql_compiler/schema_transformation/rename_schema.py
+++ graphql_compiler/schema_transformation/rename_schema.py
@@ -1125,8 +1125,10 @@ class CascadingSuppressionCheckVisitor(Visitor):
             return IDLE
         # At a field of a type that is not the query type.
         # A field must be suppressed if its type depends on an unqueryable type. There are two ways
-        # for a type to become unqueryable: either the type itself was suppressed, or the type is an
-        # interface and another type implementing the interface was suppressed. The field can either
+        # for a type to become unqueryable: 
+        #    - the type itself was suppressed
+        #    - the type is an interface and another type implementing the interface was suppressed
+        # The field can either
         # be suppressed by suppressing the current type altogether or suppressing just that
         # individual field.
         field_name = node.name.value
