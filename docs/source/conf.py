a5af1b1fe846f9df9a38445a1789cb89995a4062
Leon Wu <lwgithub@gmail.com>
2021-04-22 11:55:06 -0400

    Revert "type-hint docs"
    This reverts commit b7854ae37eb4a1cc4c099abcf66b53257c1bba3c.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,6 @@
 
 
 # -- Project information -----------------------------------------------------
-from typing import List
 
 
 project = "GraphQL Compiler"
@@ -33,7 +32,7 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions: List[str] = []
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -41,7 +40,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns: List[str] = []
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
